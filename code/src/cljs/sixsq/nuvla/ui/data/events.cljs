6d2c962e5b8a4534c4aa528d0831fe7535870cca
Charles Loomis <cal@sixsq.com>
2019-03-06 14:16:46 +0100

    update data resource names

--- code/src/cljs/sixsq/nuvla/ui/data/events.cljs
+++ code/src/cljs/sixsq/nuvla/ui/data/events.cljs
@@ -12,10 +12,10 @@
 
 
 (defn fetch-data-cofx
-  [credentials client time-period-filter cloud-filter full-text-search datasets]
+  [credentials client time-period-filter cloud-filter full-text-search data-sets]
   (if (empty? credentials)
     {}
-    {::fx/fetch-data [client time-period-filter cloud-filter full-text-search (vals datasets)
+    {::fx/fetch-data [client time-period-filter cloud-filter full-text-search (vals data-sets)
                       #(dispatch [::set-data %1 %2])]}))
 
 
@@ -24,12 +24,12 @@
   (fn [{{:keys [::client-spec/client
                 ::spec/cloud-filter
                 ::spec/credentials
-                ::spec/datasets
+                ::spec/data-sets
                 ::spec/full-text-search] :as db} :db} [_ time-period]]
     (let [time-period-filter (utils/create-time-period-filter time-period)]
       (merge {:db (assoc db ::spec/time-period time-period
                             ::spec/time-period-filter time-period-filter)}
-             (fetch-data-cofx credentials client time-period-filter cloud-filter full-text-search datasets)))))
+             (fetch-data-cofx credentials client time-period-filter cloud-filter full-text-search data-sets)))))
 
 
 (reg-event-fx
@@ -37,37 +37,37 @@
   (fn [{{:keys [::client-spec/client
                 ::spec/cloud-filter
                 ::spec/credentials
-                ::spec/datasets
+                ::spec/data-sets
                 ::spec/time-period-filter] :as db} :db} [_ full-text-search]]
     (let [full-text-query (when (and full-text-search (not (str/blank? full-text-search)))
                             (str "fulltext=='" full-text-search "*'"))]
       (merge {:db (assoc db ::spec/full-text-search full-text-query)}
-             (fetch-data-cofx credentials client time-period-filter cloud-filter full-text-query datasets)))))
+             (fetch-data-cofx credentials client time-period-filter cloud-filter full-text-query data-sets)))))
 
 
 (reg-event-db
-  ::set-service-offers
-  (fn [db [_ service-offers]]
-    (assoc db ::spec/service-offers service-offers)))
+  ::set-data-records
+  (fn [db [_ data-records]]
+    (assoc db ::spec/data-records data-records)))
 
 
 (reg-event-db
   ::set-data
-  (fn [db [_ dataset-id response]]
+  (fn [db [_ data-set-id response]]
     (let [doc-count (get-in response [:aggregations :count:id :value])
           total-bytes (get-in response [:aggregations :sum:data:bytes :value])
-          service-offer-ids (mapv :id (:resources response))]
+          data-record-ids (mapv :id (:resources response))]
       (-> db
-          (update ::spec/counts assoc dataset-id doc-count)
-          (update ::spec/sizes assoc dataset-id total-bytes)
-          (update ::spec/service-offers-by-dataset assoc (keyword dataset-id) service-offer-ids)))))
+          (update ::spec/counts assoc data-set-id doc-count)
+          (update ::spec/sizes assoc data-set-id total-bytes)
+          (update ::spec/data-records-by-data-set assoc (keyword data-set-id) data-record-ids)))))
 
 
 (reg-event-fx
   ::set-credentials
   (fn [{{:keys [::client-spec/client
                 ::spec/time-period-filter
-                ::spec/datasets
+                ::spec/data-sets
                 ::spec/full-text-search] :as db} :db} [_ {:keys [resources]}]]
     (let [cloud-filter (utils/create-cloud-filter resources)]
       (when client
@@ -75,7 +75,7 @@
                               ::spec/cloud-filter cloud-filter
                               ::spec/counts nil
                               ::spec/sizes nil)}
-               (fetch-data-cofx resources client time-period-filter cloud-filter full-text-search datasets))))))
+               (fetch-data-cofx resources client time-period-filter cloud-filter full-text-search data-sets))))))
 
 
 (reg-event-fx
@@ -83,8 +83,8 @@
   (fn [{{:keys [::client-spec/client] :as db} :db} _]
     (when client
       {:db                  (assoc db ::spec/credentials nil)
-       ::cimi-api-fx/search [client :credential {:filter "type^='cloud-cred'"
-                                                 :select "id, name, connector"}
+       ::cimi-api-fx/search [client :credential {:filter "type^='infrastructure-service-swarm'"
+                                                 :select "id, name, infrastructure-service"}
                              #(dispatch [::set-credentials %])]})))
 
 
@@ -109,12 +109,12 @@
 
 (reg-event-fx
   ::open-application-select-modal
-  (fn [{{:keys [::client-spec/client
-                ::spec/datasets
-                ::spec/selected-dataset-ids] :as db} :db} _]
-    (let [selected-datasets (vals (filter (fn [[k v]] (boolean (selected-dataset-ids k))) datasets))
-          query-application (apply utils/join-and (map (keyword "dataset:applicationFilter") selected-datasets))
-          query-objects (apply utils/join-or (map (keyword "dataset:objectFilter") selected-datasets))]
+  (fn [{{:keys                                    [::client-spec/client
+                ::spec/data-sets
+                ::spec/selected-data-set-ids] :as db} :db} _]
+    (let [selected-data-sets (vals (filter (fn [[k v]] (boolean (selected-data-set-ids k))) data-sets))
+          query-application (apply utils/join-and (map :module-filter selected-data-sets))
+          query-objects (apply utils/join-or (map :data-record-filter selected-data-sets))]
       {:db                  (assoc db ::spec/application-select-visible? true
                                       ::spec/loading-applications? true
                                       ::spec/selected-application-id nil
@@ -130,6 +130,7 @@
     {:db (assoc db ::spec/applications nil
                    ::spec/application-select-visible? false)}))
 
+
 (reg-event-fx
   ::delete-deployment
   (fn [{{:keys [::client-spec/client
@@ -138,34 +139,33 @@
 
 
 (reg-event-fx
-  ::set-datasets
+  ::set-data-sets
   (fn [{{:keys [::client-spec/client
                 ::spec/credentials
                 ::spec/cloud-filter
                 ::spec/time-period-filter
-                ::spec/datasets
+                ::spec/data-sets
                 ::spec/full-text-search] :as db} :db} [_ {:keys [resources]}]]
     (let []
-      (assoc db ::spec/datasets datasets))
-    (let [datasets (into {} (map (juxt :id identity) resources))]
+      (assoc db ::spec/data-sets data-sets))
+    (let [data-sets (into {} (map (juxt :id identity) resources))]
       (when client
         (merge {:db (assoc db ::spec/counts nil
                               ::spec/sizes nil
-                              ::spec/datasets datasets)}
-               (fetch-data-cofx credentials client time-period-filter cloud-filter full-text-search datasets))))))
+                              ::spec/data-sets data-sets)}
+               (fetch-data-cofx credentials client time-period-filter cloud-filter full-text-search data-sets))))))
 
 
 (reg-event-fx
-  ::get-datasets
+  ::get-data-sets
   (fn [{{:keys [::client-spec/client] :as db} :db} _]
     (when client
-      {:db                  (assoc db ::spec/datasets nil)
-       ::cimi-api-fx/search [client :service-offer {:filter "resource:type='DATASET'"}
-                             #(dispatch [::set-datasets %])]})))
+      {:db                  (assoc db ::spec/data-sets nil)
+       ::cimi-api-fx/search [client :data-set {} #(dispatch [::set-data-sets %])]})))
 
 
 (reg-event-db
-  ::toggle-dataset-id
-  (fn [{:keys [::spec/selected-dataset-ids] :as db} [_ id]]
-    (let [f (if (get selected-dataset-ids id) disj conj)]
-      (assoc db ::spec/selected-dataset-ids (f selected-dataset-ids id)))))
+  ::toggle-data-set-id
+  (fn [{:keys [::spec/selected-data-set-ids] :as db} [_ id]]
+    (let [f (if (get selected-data-set-ids id) disj conj)]
+      (assoc db ::spec/selected-data-set-ids (f selected-data-set-ids id)))))

55ac3cb09cce07f4364c56eba9367313af4a7b57
khaled basbous <khaled@sixsq.com>
2019-05-06 17:00:06 +0200

    better management of session with multiple tabs

--- code/src/cljs/sixsq/nuvla/ui/authn/effects.cljs
+++ code/src/cljs/sixsq/nuvla/ui/authn/effects.cljs
@@ -2,10 +2,19 @@
   (:require [re-frame.core :refer [dispatch reg-fx]]
             [sixsq.nuvla.ui.utils.time :as time]))
 
+
+(def timeout-id (atom nil))
+
+
 (reg-fx
   ::automatic-logout-at-session-expiry
   (fn [[{:keys [expiry] :as session}]]
-    (let [remaining-milliseconds (->> expiry (time/delta-milliseconds (time/now)) int)]
-      (js/setTimeout #(do
-                        (dispatch [:sixsq.nuvla.ui.authn.events/logout])
-                        (dispatch [:sixsq.nuvla.ui.authn.events/open-modal :login])) remaining-milliseconds))))
+    (let [logout-callback       (fn []
+                                  (dispatch [:sixsq.nuvla.ui.authn.events/logout])
+                                  (dispatch [:sixsq.nuvla.ui.authn.events/open-modal :login]))
+          remaining-time-millis (->> expiry (time/delta-milliseconds (time/now)) int)]
+      (if (pos-int? remaining-time-millis)
+        (do (js/clearTimeout @timeout-id)
+            (reset! timeout-id (js/setTimeout logout-callback remaining-time-millis))
+            (dispatch [:sixsq.nuvla.ui.authn.events/open-modal nil]))
+        (logout-callback)))))

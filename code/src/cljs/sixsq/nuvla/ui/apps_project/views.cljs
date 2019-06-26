cec6b7af62c7ddb3028e2a408d5faafcdaa7605c
khaled basbous <khaled@sixsq.com>
2019-06-26 10:21:02 +0200

    cleanup, issue with when to reload ui-acl

--- code/src/cljs/sixsq/nuvla/ui/apps_project/views.cljs
+++ code/src/cljs/sixsq/nuvla/ui/apps_project/views.cljs
@@ -84,9 +84,11 @@
          [:h2 {:style {:display :inline}}
           [ui/Icon {:name "folder"}]
           parent (when (not-empty parent) "/") name]
-         [acl/AclButton {:acl       (get @module-common ::apps-spec/acl)
-                         :on-change #(do (dispatch [::apps-events/acl %])
-                                         (dispatch [::main-events/changes-protection? true]))
+         [acl/AclButton {:id        (:id @module)
+                         :acl       (get @module-common ::apps-spec/acl)
+                         :on-change #(do
+                                       (dispatch [::apps-events/acl %])
+                                       (dispatch [::main-events/changes-protection? true]))
                          :read-only (not editable?)}]
          [apps-views-detail/control-bar]
          [summary]

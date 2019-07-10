73d2980644e8eb08fa8b6f70f02155bc6c8542eb
Khaled Basbous <khaled@sixsq.com>
2019-07-10 13:44:00 +0200

    Apps Component - Unclear how to fill docker image fields (#203)

--- code/src/cljs/sixsq/nuvla/ui/apps_component/spec.cljs
+++ code/src/cljs/sixsq/nuvla/ui/apps_component/spec.cljs
@@ -1,7 +1,7 @@
 (ns sixsq.nuvla.ui.apps-component.spec
   (:require-macros [sixsq.nuvla.ui.utils.spec :refer [only-keys]])
   (:require [clojure.spec.alpha :as s]
-            [sixsq.nuvla.ui.apps.spec :as apps-spec]
+            [sixsq.nuvla.ui.utils.spec :as spec-utils]
             [taoensso.timbre :as log]))
 
 
@@ -17,7 +17,7 @@
 
 ; Image
 
-(s/def ::image-name apps-spec/nonblank-string)
+(s/def ::image-name spec-utils/nonblank-string)
 (s/def ::repository (s/nilable string?))
 (s/def ::registry (s/nilable string?))
 (s/def ::tag (s/nilable string?))
@@ -45,13 +45,13 @@
 
 (def env-var-regex #"^[A-Z_]+$")
 (def reserved-env-var-regex #"NUVLA_.*")
-(s/def ::env-name (s/and apps-spec/nonblank-string
+(s/def ::env-name (s/and spec-utils/nonblank-string
                          #(re-matches env-var-regex %)
                          #(not (re-matches reserved-env-var-regex %))))
 
-(s/def ::env-description (s/nilable apps-spec/nonblank-string))
+(s/def ::env-description (s/nilable spec-utils/nonblank-string))
 
-(s/def ::env-value (s/nilable apps-spec/nonblank-string))
+(s/def ::env-value (s/nilable spec-utils/nonblank-string))
 
 (s/def ::env-required boolean?)
 
@@ -65,9 +65,9 @@
 
 ; Volumes (mounts)
 
-(s/def ::mount-source apps-spec/nonblank-string)
+(s/def ::mount-source spec-utils/nonblank-string)
 
-(s/def ::mount-target apps-spec/nonblank-string)
+(s/def ::mount-target spec-utils/nonblank-string)
 
 (s/def ::mount-read-only boolean?)
 
@@ -83,14 +83,14 @@
 (s/def ::mounts (s/map-of any? (s/merge ::mount)))
 
 
-(s/def ::input-value apps-spec/nonblank-string)
+(s/def ::input-value spec-utils/nonblank-string)
 
 
 ; URLs
 
-(s/def ::url-name apps-spec/nonblank-string)
+(s/def ::url-name spec-utils/nonblank-string)
 
-(s/def ::url apps-spec/nonblank-string)
+(s/def ::url spec-utils/nonblank-string)
 
 (s/def ::single-url (s/keys :req [::url-name
                                   ::url]))
@@ -100,9 +100,9 @@
 
 ; Output parameters
 
-(s/def ::output-parameter-name apps-spec/nonblank-string)
+(s/def ::output-parameter-name spec-utils/nonblank-string)
 
-(s/def ::output-parameter-description apps-spec/nonblank-string)
+(s/def ::output-parameter-description spec-utils/nonblank-string)
 
 (s/def ::output-parameter (s/keys :req [::output-parameter-name
                                         ::output-parameter-description]))

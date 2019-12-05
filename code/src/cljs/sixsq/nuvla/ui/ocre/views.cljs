81c0246529fea4d9ec0af0513d241c17b7d50313
cjdcordeiro <cristovao.cordeiro@sixsq.com>
2019-12-05 17:25:32 +0100

    fix default value ocre for distrib

--- code/src/cljs/sixsq/nuvla/ui/ocre/views.cljs
+++ code/src/cljs/sixsq/nuvla/ui/ocre/views.cljs
@@ -94,7 +94,7 @@
             :amount (js/parseFloat v)
             :state (str/upper-case v)
             :currency (str/upper-case v)
-            :distributor (or v "OCRE")
+            :distributor (if (blank? v) "OCRE" v)
             v)]
     [k v]))
 

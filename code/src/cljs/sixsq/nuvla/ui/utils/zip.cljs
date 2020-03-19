335b335da3deec0d4eb8faa2d9b40a1f4bf2dd34
khaled basbous <khaled@sixsq.com>
2020-03-19 11:34:49 +0100

    Edge page – Create nuvlabox modal generate a single zip for NuvlaBox engine

---
+++ code/src/cljs/sixsq/nuvla/ui/utils/zip.cljs
@@ -0,0 +1,12 @@
+(ns sixsq.nuvla.ui.utils.zip
+  (:require
+    ["jszip" :as jszip]))
+
+(defn create
+  [files callback]
+  (let [zip (jszip)]
+    (doseq [{:keys [name file]} files]
+      (.file zip name file))
+    (-> zip
+        (.generateAsync #js{"type" "base64", "compression" "STORE"})
+        (.then #(callback (str "data:application/zip;base64," %))))))

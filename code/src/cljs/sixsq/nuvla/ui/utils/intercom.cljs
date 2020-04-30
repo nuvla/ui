f54395fce6a820f85370242c8b812bbf0d0572bf
khaled basbous <khaled@sixsq.com>
2020-04-30 10:04:47 +0200

    Add Intercom dependency

---
+++ code/src/cljs/sixsq/nuvla/ui/utils/intercom.cljs
@@ -0,0 +1,10 @@
+(ns sixsq.nuvla.ui.utils.intercom
+  (:require
+    ["react-intercom" :as react-intercom]
+    [reagent.core :as reagent]))
+
+
+(def intercom-api react-intercom/IntercomAPI)
+
+
+(def Intercom (reagent/adapt-react-class react-intercom/default))

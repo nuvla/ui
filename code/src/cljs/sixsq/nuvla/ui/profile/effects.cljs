eb7669820b26f51af2c7d10b22710b7d4c8252c6
khaled basbous <khaled@sixsq.com>
2020-06-11 07:42:04 +0200

    create-payment-method effect uncomment

--- code/src/cljs/sixsq/nuvla/ui/profile/effects.cljs
+++ code/src/cljs/sixsq/nuvla/ui/profile/effects.cljs
@@ -6,18 +6,13 @@
 (reg-fx
   ::create-payment-method
   (fn [[stripe data callback]]
-    (js/console.error "::create-payment-method " stripe data callback)
-    (js/console.error "::create-payment-method " (.-createPaymentMethod stripe))
-    (js/console.error "::create-payment-method " (. stripe -createPaymentMethod))
-
-    #_(-> ((.-createPaymentMethod stripe) data)
+    (-> ((.-createPaymentMethod stripe) data)
         (.then (fn [result] (callback result))))))
 
 
 (reg-fx
   ::confirm-card-setup
   (fn [[stripe client-secret data callback]]
-    (js/console.error "::confirm-card-setup " stripe)
     (-> (. stripe confirmCardSetup client-secret data)
         (.then (fn [result]
                  (callback result))))))
@@ -26,7 +21,6 @@
 (reg-fx
   ::confirm-sepa-debit-setup
   (fn [[stripe client-secret data callback]]
-    (js/console.error "::confirm-sepa-debit-setup" stripe)
     (-> (. stripe confirmSepaDebitSetup client-secret data)
         (.then (fn [result]
                  (callback result))))))

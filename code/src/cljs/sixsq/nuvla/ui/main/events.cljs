66f8825123c0045348a862d40ced0ff55485a73c
khaled basbous <khaled@sixsq.com>
2020-06-11 08:35:35 +0200

    disable precompress of resources and more logs

--- code/src/cljs/sixsq/nuvla/ui/main/events.cljs
+++ code/src/cljs/sixsq/nuvla/ui/main/events.cljs
@@ -293,6 +293,8 @@
   ::stripe-loaded
   (fn [db [_ stripe]]
     (js/console.log stripe)
+    (js/console.error "::stripe-loaded " (. stripe -createPaymentMethod))
+    (js/console.error "::stripe-loaded " (js->clj stripe))
     (assoc db ::spec/stripe stripe)))
 
 

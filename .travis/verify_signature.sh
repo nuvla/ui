8edb80848be17ebe3380df8a8cd324184a56bc46
Lionel <lionel@sixsq.com>
2020-07-29 13:23:00 +0200

    WIP: removed --import-options show-only because not available on older GPG versions

--- .travis/verify_signature.sh
+++ .travis/verify_signature.sh
@@ -3,4 +3,5 @@
 gpg_pubkey=".travis/sixsq.gpg_pubkey.bin"
 signed_file="code/resources/public/ui/downloads/nuvlabox-self-registration.py.gpg"
 
-gpg --dry-run --import-options show-only --batch --status-fd=1 --no-sig-cache --no-default-keyring --keyring "${gpg_pubkey}" --verify "${signed_file}"
+gpg --version
+gpg --dry-run --batch --status-fd=1 --no-sig-cache --no-default-keyring --keyring "${gpg_pubkey}" --verify "${signed_file}"

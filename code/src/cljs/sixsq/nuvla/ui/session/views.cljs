d25f9dfbd06d3ecb536e05d37e910cb42df11a81
Marc-Elian Begin <meb@mebster.me>
2020-08-12 16:08:59 +0200

    Fix layout on smaller screens

--- code/src/cljs/sixsq/nuvla/ui/session/views.cljs
+++ code/src/cljs/sixsq/nuvla/ui/session/views.cljs
@@ -230,13 +230,10 @@
      [ui/Image {:alt      "logo"
                 :src      "/ui/images/nuvla-logo.png"
                 :size     "medium"
-                :style    {:margin-top    "10px"
-                           :margin-bottom "0px"}
                 :centered false}]
      [:br]
 
-     [:div {:style {:margin-top  40
-                    :line-height "normal"
+     [:div {:style {:line-height "normal"
                     :font-size   "2em"}}
       (@tr [:edge-platform-as-a-service])]
      [:br]
@@ -313,7 +310,7 @@
               :style     {:margin           0
                           :background-color "white"}}
 
-     [ui/GridColumn {:style {:background-image    "url(/ui/images/volumlight.png)"
+     [ui/GridColumn {:style {:background-image    "url(/ui/images/session.png)"
                              :background-size     "cover"
                              :background-position "left"
                              :background-repeat   "no-repeat"

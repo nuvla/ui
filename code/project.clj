41ffd0918b534cb7e8b37a7a7390fe1ee8467ba7
khaled basbous <khaled@sixsq.com>
2020-10-15 19:29:47 +0200

    release 2.8.1

--- code/project.clj
+++ code/project.clj
@@ -1,8 +1,8 @@
 (def parent-version "6.7.4")
 (def sixsq-nuvla-api-version "2.0.8")
-(def version "2.8.1-SNAPSHOT")
+(def version "2.8.1")
 
-(defproject sixsq.nuvla.ui/code "2.8.1-SNAPSHOT"
+(defproject sixsq.nuvla.ui/code "2.8.1"
 
   :description "Web Browser User Interface for Nuvla"
 

d503db5143a3a0e1a4bbbf5805d65a21faabdf23
Lionel <lionel@sixsq.com>
2020-11-04 21:10:44 +0100

    Deployment - show an information message about VPN when the infrastructure has a private IP. #449

--- code/src/cljs/sixsq/nuvla/ui/utils/spec.cljs
+++ code/src/cljs/sixsq/nuvla/ui/utils/spec.cljs
@@ -28,16 +28,25 @@
 
 (def email-regex #"^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
-
 (defn email? [s] (re-matches email-regex s))
 
 (s/def ::email (s/and string? email?))
+
 
 (def timestamp-regex #"^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(\.[0-9]{1,3})?(Z)?$")
 
 (defn timestamp? [s] (re-matches timestamp-regex s))
 
 (s/def ::timestamp (s/and string? timestamp?))
+
+
+(def ipv4-regex #"^((25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[0-9])(\.(?!$)|$)){4}$")
+
+(def private-ipv4-regex #"(^127\.)|(^10\.)|(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)|(^192\.168\.)")
+
+(defn ipv4? [s] (not (nil? (re-matches ipv4-regex s))))
+
+(defn private-ipv4? [s] (and (ipv4? s) (not (nil? (re-find private-ipv4-regex s)))))
 
 
 (s/def ::username nonblank-string)

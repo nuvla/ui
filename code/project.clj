8e023a3fe935ef8e56393d7882066940d0fee9d1
khaled basbous <khaled@sixsq.com>
2020-11-16 10:11:45 +0100

    next development version

--- code/project.clj
+++ code/project.clj
@@ -1,8 +1,8 @@
 (def parent-version "6.7.4")
 (def sixsq-nuvla-api-version "2.0.8")
-(def version "2.10.0")
+(def version "2.10.1-SNAPSHOT")
 
-(defproject sixsq.nuvla.ui/code "2.10.0"
+(defproject sixsq.nuvla.ui/code "2.10.1-SNAPSHOT"
 
   :description "Web Browser User Interface for Nuvla"
 

34852b3ed7d4417cb1e3b0bf4bc6f416fae83e24
khaled basbous <khaled@sixsq.com>
2021-06-02 15:12:57 +0200

    disable debug in advanced optimizations, use api client latest

--- code/project.clj
+++ code/project.clj
@@ -1,5 +1,5 @@
 (def parent-version "6.7.5")
-(def sixsq-nuvla-api-version "2.0.9")
+(def sixsq-nuvla-api-version "2.0.10")
 (def version "2.18.2-SNAPSHOT")
 
 (defproject sixsq.nuvla.ui/code "2.18.2-SNAPSHOT"

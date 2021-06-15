1513353a2679f63c8c8f47298beb04356dbe8402
Lionel <lionel@sixsq.com>
2021-06-15 11:14:23 +0200

    switch group dropdown: sort groups and remove "group/" prefix.

--- code/src/cljs/sixsq/nuvla/ui/session/utils.cljs
+++ code/src/cljs/sixsq/nuvla/ui/session/utils.cljs
@@ -1,13 +1,13 @@
-(ns sixsq.nuvla.ui.session.utils)
+(ns sixsq.nuvla.ui.session.utils
+  (:require
+    [clojure.string :as str]))
 
 (def ^:const user-tmpl-email-password "user-template/email-password")
 (def ^:const user-tmpl-email-invitation "user-template/email-invitation")
 (def ^:const session-tmpl-password "session-template/password")
 (def ^:const session-tmpl-password-reset "session-template/password-reset")
 
-
-
-
-
-
+(defn remove-group-prefix
+  [account]
+  (str/replace-first account #"^group/" ""))
 

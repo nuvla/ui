0f774a766179775b3863d29bc8f88a76efd7d38f
Mario Trost <mario.trost@bluewin.ch>
2022-09-30 19:50:56 +0200

    Revert "Adds all public assets"
    This reverts commit 3fa214357be6ca61324946c70be81356bd351c45.

--- code/functions/api/[[path]].js
+++ code/functions/api/[[path]].js
@@ -8,24 +8,8 @@ export async function onRequest(context) {
     next, // used for middleware or to fetch assets
     data, // arbitrary space for passing data between middlewares
   } = context;
+
   const url = new URL(request.url);
-  let apiUrl = 'https://nuvla.io/' + url.pathname;
 
-  // Rewrite request to point to API URL. This also makes the request mutable
-  // so you can add the correct Origin header to make the API server think
-  // that this request is not cross-site.
-  request = new Request(apiUrl, request);
-  request.headers.set('Origin', new URL(apiUrl).origin);
-  let response = await fetch(request);
-
-  // Recreate the response so you can modify the headers
-  response = new Response(response.body, response);
-
-  // Set CORS headers
-  response.headers.set('Access-Control-Allow-Origin', url.origin);
-
-  // Append to/Add Vary header so browser will cache response correctly
-  response.headers.append('Vary', 'Origin');
-
-  return response;
+  return fetch(`https://nuvla.io${url.pathname}`, request);
 }

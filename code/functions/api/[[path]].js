c9beca89f2a966a2b5683e8708b8c1c82ed6fe7e
Mario Trost <mario.trost@bluewin.ch>
2022-09-30 21:14:41 +0200

    onRequestOptions: allow cors and methods

--- code/functions/api/[[path]].js
+++ code/functions/api/[[path]].js
@@ -1,4 +1,4 @@
-export async function onRequest(context) {
+async function onRequest(context) {
   // Contents of context object
   let {
     request, // same as existing Worker API
@@ -28,4 +28,55 @@ export async function onRequest(context) {
   response.headers.append('Vary', 'Origin');
 
   return response;
+}
+
+// Cloudflare supports the GET, POST, HEAD, and OPTIONS methods from any origin,
+// and allow any header on requests. These headers must be present
+// on all responses to all CORS preflight requests. In practice, this means
+// all responses to OPTIONS requests.
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': '*',
+  'Access-Control-Max-Age': '86400',
+};
+
+export let onRequestGet = onRequest
+export let onRequestPost= onRequest
+export let onRequestPut = onRequest
+export let onRequestPatch = onRequest
+export let onRequestDelete = onRequest
+export let onRequestHead = onRequest
+
+
+export async onRequestOptions(request) {
+  // Make sure the necessary headers are present
+  // for this to be a valid pre-flight request
+  let headers = request.headers;
+  if (
+    headers.get('Origin') !== null &&
+    headers.get('Access-Control-Request-Method') !== null &&
+    headers.get('Access-Control-Request-Headers') !== null
+  ) {
+    // Handle CORS pre-flight request.
+    // If you want to check or reject the requested method + headers
+    // you can do that here.
+    let respHeaders = {
+      ...corsHeaders,
+      // Allow all future content Request headers to go back to browser
+      // such as Authorization (Bearer) or X-Client-Name-Version
+      'Access-Control-Allow-Headers': request.headers.get('Access-Control-Request-Headers'),
+    };
+
+    return new Response(null, {
+      headers: respHeaders,
+    });
+  } else {
+    // Handle standard OPTIONS request.
+    // If you want to allow other HTTP Methods, you can do that here.
+    return new Response(null, {
+      headers: {
+        Allow: 'GET, HEAD, POST, OPTIONS, PUT, DELETE, PATCH',
+      },
+    });
+  }
 }

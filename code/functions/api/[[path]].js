3defc3fa9a043bfb1600f0fe21bd1c1eb1457424
Mario Trost <mario.trost@bluewin.ch>
2022-10-04 14:29:25 +0200

    Removes unused parameter

--- code/functions/api/[[path]].js
+++ code/functions/api/[[path]].js
@@ -6,9 +6,6 @@ export async function onRequest(context) {
     request, // same as existing Worker API                           -> needed
     env, // same as existing Worker API                               -> needed for different api backends than nuvla.io
     params, // if filename includes [id] or [[path]]
-    waitUntil, // same as ctx.waitUntil in existing Worker API
-    next, // used for middleware or to fetch assets
-    data, // arbitrary space for passing data between middlewares
   } = context;
   const url = new URL(request.url);
 
--- code/functions/ui/[[path]].js
+++ code/functions/ui/[[path]].js
@@ -4,9 +4,6 @@ export async function onRequest(context) {
     request, // same as existing Worker API
     env, // same as existing Worker API
     params, // if filename includes [id] or [[path]]
-    waitUntil, // same as ctx.waitUntil in existing Worker API
-    next, // used for middleware or to fetch assets
-    data, // arbitrary space for passing data between middlewares
   } = context;
   const url = new URL(request.url);
 

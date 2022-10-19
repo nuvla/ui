b9633341501153930e819faa1f1909dd436c8a8f
Mario Trost <mario.trost@bluewin.ch>
2022-10-19 16:03:41 +0200

    Tests marketplace to only show published apps

--- code/tests/loggedin/marketplace.spec.js
+++ code/tests/loggedin/marketplace.spec.js
@@ -1,33 +1,22 @@
 import { test, expect } from '@playwright/test';
 
-test.skip('Marketplace shows only published apps', async ({ page, context }, { project, config }) => {
+test('Marketplace shows only published apps', async ({ page, context }, { project, config }) => {
   const { baseURL } = config.projects[0].use;
   const marketplaceUrl = baseURL + '/ui/apps';
 
   await page.goto(marketplaceUrl);
-  // await page.pause();
-  // await page.waitForRequest((request) => request.postDataJSON()?.filter?.includes?.('published=true'));
-  // await page.waitForResponse((resp) => resp.url().includes('module'));
-  // await page.waitForResponse((resp) => resp.url().includes('module'));
-  // await page.waitForTimeout(5000);
-  // const appCards = await page.waitForSelector('a.ui.card', { state: 'visible' });
-  // const appCards = page.locator('a.ui.card');
-  const appCards = page.locator('a.ui.card');
-  await appCards.nth(0).waitFor();
-  // await appCards.click();
-  // console.log(page.url());
-  // await page.pause();
-  // console.log({ appCards });
-  const linksCount = await appCards.count();
-  console.log({ linksCount });
+  const appCards = await page.locator('a.ui.card').nth(0).waitFor();
+  const elements = await page.$$('a.ui.card');
 
-  // TODO: check that all apps are published
-  // for (let i = 0; i < linksCount; i++) {
-  //   const hrefs = [];
-  //   for (let i = 0; i < linksCount; i++) {
-  //     hrefs.push(await appCards.nth(i).getAttribute('href'));
-  //   }
-
-  //   console.log(hrefs);
-  // }
+  let hrefs = await Promise.all(elements.map(async (elem) => elem.getAttribute('href')));
+  expect(hrefs.length).toBeGreaterThan(0);
+  for (const href of hrefs) {
+    const appUrlToVisit = baseURL + '/ui/' + href;
+    await page.goto(appUrlToVisit);
+    console.log('checking app url:', appUrlToVisit);
+    await page
+      .locator('a', { hasText: /\bun-publish\b/i })
+      .nth(0)
+      .waitFor({ timeout: 5000 });
+  }
 });

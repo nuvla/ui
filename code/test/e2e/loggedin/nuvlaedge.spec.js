4dfd3315f73f47ae8dcae2533ef65b3aa71a3dcb
khaled basbous <khaled@sixsq.com>
2022-11-18 10:56:01 +0100

    E2E nuvlaedge - fix tests to align with api changes

--- code/test/e2e/loggedin/nuvlaedge.spec.js
+++ code/test/e2e/loggedin/nuvlaedge.spec.js
@@ -59,8 +59,8 @@ test('NuvlaEdge creation and deletion', async ({ page, context }, { project, con
   await page.keyboard.press(`${modifier}+KeyV`);
   await page.keyboard.press(`${modifier}+KeyV`);
   const cronjob = await page.evaluate(() => document.querySelector('div').textContent);
-  expect(cronjob.startsWith('* 0 * * * ')).toBeTruthy();
-  for (const envVar of ['NUVLABOX_API_KEY', 'NUVLABOX_API_SECRET', 'NUVLA_ENDPOINT']) {
+  expect(cronjob.startsWith('0 * * * * ')).toBeTruthy();
+  for (const envVar of ['NUVLABOX_API_KEY', 'NUVLABOX_API_SECRET', 'NUVLA_API_ENDPOINT']) {
     const testRegex = new RegExp(` (${envVar})=`);
     const [_, matchedEnvVar] = cronjob.match(testRegex) || [];
 

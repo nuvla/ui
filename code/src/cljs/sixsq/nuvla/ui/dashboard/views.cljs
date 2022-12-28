7b413c85fdf5f5688c46bf9d2d4ba248b39755c8
Mario Trost <mario.trost@bluewin.ch>
2022-12-28 16:12:07 +0100

    Styles deployments card on Dashboard page

--- code/src/cljs/sixsq/nuvla/ui/dashboard/views.cljs
+++ code/src/cljs/sixsq/nuvla/ui/dashboard/views.cljs
@@ -7,15 +7,15 @@
     [sixsq.nuvla.ui.dashboard.events :as events]
     [sixsq.nuvla.ui.dashboard.utils :as utils]
     [sixsq.nuvla.ui.deployments.subs :as deployments-subs]
-    [sixsq.nuvla.ui.deployments.views :as deployments-views]
+    [sixsq.nuvla.ui.deployments.utils :as deployments-utils]
     [sixsq.nuvla.ui.edges.subs :as edges-subs]
     [sixsq.nuvla.ui.edges.views :as edges-views]
     [sixsq.nuvla.ui.history.events :as history-events]
     [sixsq.nuvla.ui.i18n.subs :as i18n-subs]
     [sixsq.nuvla.ui.main.components :as components]
     [sixsq.nuvla.ui.panel :as panel]
+    [sixsq.nuvla.ui.utils.general :as general-utils]
     [sixsq.nuvla.ui.utils.semantic-ui :as ui]
-    [sixsq.nuvla.ui.utils.semantic-ui-extensions :as uix]
     [sixsq.nuvla.ui.utils.style :as utils-style]))
 
 
@@ -55,6 +55,49 @@
                                 (dispatch [::history-events/navigate resource]))}]]))
 
 
+
+(defn Statistic
+  [{:keys [value icon class label target positive-color]
+    :or {positive-color "black"}}]
+  (let [color (if (pos? value) positive-color "grey")
+        {:keys [resource tab-event]} target
+        interactive? (or tab-event resource)]
+    [ui/Statistic {:style    {:cursor (when interactive? "pointer")}
+                   :color    color
+                   :class    (conj [(when interactive? "slight-up")] class)
+                   :on-click #(do
+                                (when tab-event
+                                  (dispatch tab-event))
+                                (when resource
+                                  (dispatch [::history-events/navigate resource])))}
+     [ui/Icon {:className (str "icons " icon)}]
+     [ui/StatisticValue (or value "-")]
+     [ui/StatisticLabel label]]))
+
+
+(defn StatisticStates
+  [summary-subs]
+  (let [summary       (subscribe [summary-subs])
+        terms         (general-utils/aggregate-to-map
+                       (get-in @summary [:aggregations :terms:state :buckets]))
+        started       (:STARTED terms 0)
+        starting      (:STARTING terms 0)
+        created       (:CREATED terms 0)
+        stopped       (:STOPPED terms 0)
+        error         (:ERROR terms 0)
+        pending       (:PENDING terms 0)
+        starting-plus (+ starting created pending)
+        total         (:count @summary)]
+    [ui/GridColumn {:class "wide"
+                    :style {:padding "0.2rem"}}
+     [ui/StatisticGroup {:size  "tiny"
+                         :style {:justify-content "center"}}
+      [Statistic {:value total :icon "fa-light fa-rocket-launch" :label "TOTAL" :positive-color "grey"}]
+      [Statistic {:value started :icon (deployments-utils/state->icon deployments-utils/STARTED) :label deployments-utils/STARTED :positive-color "grey"}]
+      [Statistic {:value starting-plus :icon (deployments-utils/state->icon deployments-utils/STARTING) :label deployments-utils/STARTING :positive-color "grey"}]
+      [Statistic {:value stopped :icon (deployments-utils/state->icon deployments-utils/STOPPED) :label deployments-utils/STOPPED :positive-color "grey"}]
+      [Statistic {:value error :icon (deployments-utils/state->icon deployments-utils/ERROR) :label deployments-utils/ERROR :positive-color "grey"}]]]))
+
 ; TODO: reduce duplication with deployment-views/DeploymentsOverviewSegment
 (defn TabOverviewDeployments
   []
@@ -71,30 +114,13 @@
 
      [:h4 {:class "ui-header"} [ui/Icon {:name icon}] (str/upper-case (@tr [:deployments]))]
 
-     [deployments-views/StatisticStates false ::deployments-subs/deployments-summary-all]
+     [StatisticStates ::deployments-subs/deployments-summary-all]
 
      [ui/Button {:class    "center"
                  :content  "Show me"
                  :on-click #(do (when (and tab-event tab-key)
                                   (dispatch [tab-event tab-key]))
                                 (dispatch [::history-events/navigate resource]))}]]))
-
-
-(defn Statistic
-  [{:keys [value icon class label target]}]
-  (let [color (if (pos? value) "black" "grey")
-        {:keys [resource tab-event]} target]
-    [ui/Statistic {:style    {:cursor "pointer"}
-                   :color    color
-                   :class    (conj ["slight-up"] class)
-                   :on-click #(do
-                                (when tab-event
-                                  (dispatch tab-event))
-                                (dispatch [::history-events/navigate resource]))}
-     [ui/Icon {:className icon}]
-     [ui/StatisticValue (or value "-")]
-     [ui/StatisticLabel label]]))
-
 
 (defn Statistics
   []

d6547f7fa658ad5bdd8a0db90c06479cd5c24d59
Mario Trost <mario.trost@bluewin.ch>
2023-01-09 17:50:54 +0100

    Removes unused function

--- code/src/cljs/sixsq/nuvla/ui/routing/utils.cljs
+++ code/src/cljs/sixsq/nuvla/ui/routing/utils.cljs
@@ -1,6 +1,5 @@
 (ns sixsq.nuvla.ui.routing.utils
   (:require [clojure.string :as str]
-            [reitit.core :as r :refer [match-by-path]]
             [reitit.frontend.easy :as rfe]
             [sixsq.nuvla.ui.config :as config]))
 
@@ -31,14 +30,6 @@
                        (str base-path (str/replace url #"^/" "")))]
     absolute-url))
 
-(defn url->route-path-params [router url]
-  (let [absolute-url (add-base-path url)
-        [path _] (str/split absolute-url #"\?")
-        match        (match-by-path router path)
-        name         (get-in match [:data :name])
-        path-params  (:path-params match)
-        query-params (decode-query-string absolute-url)]
-    [name path-params query-params]))
 
 (defn pathify
   "Takes a list of path parts, returning a string of those parts separated by '/'."
@@ -49,7 +40,7 @@
   "Takes a list of path parts, returning a string of those parts separated by '/'
    and with config/base-path appended."
   [path-parts]
-  (pathify (concat [config/base-path] path-parts)))
+  (pathify (cons config/base-path path-parts)))
 
 
 (defn trim-path

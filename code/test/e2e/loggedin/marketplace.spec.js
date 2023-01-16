59d0d92e519b0c3404a77bb741d811a54840e543
Mario Trost <mario.trost@bluewin.ch>
2023-01-16 15:26:46 +0100

    Only test first 4 apps

--- code/test/e2e/loggedin/marketplace.spec.js
+++ code/test/e2e/loggedin/marketplace.spec.js
@@ -7,15 +7,16 @@ test('Marketplace shows only published apps', async ({ page }, { project, config
   await page.getByRole('link', { name: 'Apps' }).click();
   await expect(page).toHaveURL(marketplaceUrl);
 
-  const appCards = await page.locator('a.ui.card').nth(0).waitFor({ timeout: 5000 });
+  const appCards = await page.locator('a.ui.card').nth(0).waitFor();
   const elements = await page.$$('a.ui.card');
 
   let hrefs = await Promise.all(elements.map(async (elem) => elem.getAttribute('href')));
   expect(hrefs.length).toBeGreaterThan(0);
-  for (const href of hrefs) {
+  for (const href of hrefs.slice(0, 4)) {
     const appUrlToVisit = baseURL + href;
     await page.goto(appUrlToVisit);
     await page.locator('a:has-text("Versions")').click();
+    console.log('Testing:', appUrlToVisit);
     expect(page.locator('tr.active > td:nth-child(2) > i')).toHaveClass('teal check icon');
   }
 });

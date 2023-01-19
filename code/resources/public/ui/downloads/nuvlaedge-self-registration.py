12f1a8d71ee28cc0f86955ae13eba130fee8ccde
schaubl <schaubl@users.noreply.github.com>
2023-01-19 17:30:02 +0100

    Downloads - wip: refactor nuvlaedge-self-registration.py

--- code/resources/public/ui/downloads/nuvlaedge-self-registration.py
+++ code/resources/public/ui/downloads/nuvlaedge-self-registration.py
@@ -16,7 +16,7 @@ The expected JSON schema is:
   "apikey": "credential/<uuid>",
   "apisecret": "<secret>",
   "endpoint": "<nuvla endpoint>",
-  "version": "<nuvlaedge-engine release>",
+  "version": "<nuvlaedge release>",
   "script": "<link to this script>",
   "name": "<basename nuvlaedges>",
   "description": "<base description>",
@@ -35,13 +35,18 @@ The expected JSON schema is:
 :returns NuvlaEdge UUID
 """
 
-import requests
 import argparse
+import datetime
 import json
+import platform
 import time
+import uuid
 import os
+
 from subprocess import run, PIPE, STDOUT, TimeoutExpired
-from uuid import getnode as get_mac
+
+import requests
+
 
 __copyright__ = "Copyright (C) 2020 SixSq"
 __email__ = "support@sixsq.com"
@@ -54,23 +59,59 @@ def arguments():
     """
 
     workdir = '/opt/nuvlaedge/installation'
+    workdir_old = '/opt/nuvlabox/installation'
 
     parser = argparse.ArgumentParser(description='NuvlaEdge self-registration')
-    parser.add_argument('--nuvlaedge-installation-trigger-json', dest='nb_trigger_content', default=None, metavar='JSON',
+
+    parser.add_argument('--nuvlaedge-installation-trigger-json',
+                        '--nuvlabox-installation-trigger-json',
+                        dest='nuvlaedge_trigger_content', default=None, metavar='JSON',
                         help="JSON content, as a string, of the NuvlaEdge installation trigger file")
-    parser.add_argument('--nuvlaedge-installation-dir', dest='nb_workdir', default=workdir, metavar='PATH',
+
+    parser.add_argument('--nuvlaedge-installation-dir',
+                        '--nuvlabox-installation-dir',
+                        dest='nuvlaedge_workdir', default=workdir, metavar='PATH',
                         help="Location on the filesystem where to keep the NuvlaEdge Engine installation files")
+
+    parser.add_argument('--nuvlaedge-old-installation-dir',
+                        '--nuvlabox-old-installation-dir',
+                        dest='nuvlaedge_old_workdir', default=workdir_old, metavar='PATH',
+                        help="Location on the filesystem where the previous NuvlaEdge Engine installation files were located")
 
     return parser.parse_args()
 
 
-def prepare_nuvlaedge_engine_installation(version, compose_files, workdir, keep_files=[]):
+def get_mac():
+    h = hex(uuid.getnode())[-12:]
+    return ':'.join(h[i:i+2] for i in range(0,12,2))
+
+
+def is_true(s, **kwargs):
+    """ Check if 's' string represent True else False
+
+    :param s: String to check
+    :param default: If the check cannot be done return this value or raise ValueError if not provided.
+
+    :returns True if 's' string represent True else False
+    """
+    try:
+        return s if isinstance(s, bool) \
+            else bool(s and s.lower() in ['true', '1', 't', 'y', 'yes'])
+    except:
+        message = f'Cannot check if "{s}" is True'
+        if 'default' not in kwargs:
+            raise ValueError(message)
+        else:
+            return kwargs['default']
+
+
+def prepare_nuvlaedge_installation(version, compose_files, workdir, keep_files=[]):
     """ Prepares the working environment for installing the NuvlaEdge Engine
 
-    :param version: GitHub release of the NuvlaEdge Engine
+    :param version: version number of NuvlaEdge to install
     :param compose_files: list of release assets to download
     :param workdir: path where the compose files are to be saved
-    :param keep_files: list of files that is not supposed to be modified during this preparation
+    :param keep_files: list of files that are not supposed to be modified during this preparation
 
     :returns absolute path to the NuvlaEdge Engine installer script
     """
@@ -117,14 +158,13 @@ def prepare_nuvlaedge_engine_installation(version, compose_files, workdir, keep_
     return installer_file, final_compose_files
 
 
-def install_nuvlaedge_engine(cmd, env=os.environ.copy(), timeout=600):
+def install_nuvlaedge(cmd, env=os.environ.copy(), timeout=600):
     """ Runs a command
 
     :param cmd: command to be executed
     :param env: environment to be passed
     :param timeout: time after which the command will abruptly be terminated
     """
-
     try:
         result = run(cmd, stdout=PIPE, stderr=STDOUT, env=env, input=None,
                      timeout=timeout, universal_newlines=True)
@@ -138,132 +178,164 @@ def install_nuvlaedge_engine(cmd, env=os.environ.copy(), timeout=600):
     print(result.stdout)
 
 
+def nuvla_login(api_endpoint, api_key, api_secret, insecure=False):
+    session = requests.Session()
+    session.verify = not insecure
+    login_endpoint = api_endpoint + "/session"
+    payload = {
+        "template": {
+            "href": "session-template/api-key",
+            "key": api_key,
+            "secret": api_secret
+        }
+    }
+    print("Nuvla login at {}...".format(login_endpoint))
+    response = session.post(login_endpoint, json=payload)
+    response.raise_for_status()
+    return session
+
+
+def read_env_file(env_file):
+    env_vars = []
+    with open(env_file) as f:
+        for line in f.read().splitlines():
+            if line and "=" in line:
+                name, value = line.split('=', 1)
+                env_vars[name] = value
+    return env_vars
+
+
+def generate_nuvlaedge_name_description(name_prefix, description_prefix):
+    name_suffix = ''
+
+    mac = ''
+    try:
+        mac = get_mac()
+        name_suffix = '{} {}'.format(name_suffix, mac).strip()
+    except:
+        pass
+
+    isotime = ''
+    try:
+        isotime = datetime.datetime.now().isoformat(' ')[:19]
+        name_suffix = '{} {}'.format(name_suffix, isotime).strip()
+    except:
+        pass
+
+    hostname = platform.node()
+    name_suffix = '{} {}'.format(name_suffix, hostname).strip()
+
+    name = "{} - {}".format(name_prefix, name_suffix) if name_prefix else name_suffix
+    description = "{} - MAC address: {} - Installation time: {} - Hostname: {}".format(description_prefix, mac, isotime, hostname)
+
+    return name, description
+
+
 if __name__ == "__main__":
     args = arguments()
 
-    nb_trigger_json = json.loads(args.nb_trigger_content)
-    nb_workdir = args.nb_workdir.rstrip('/')
-    env_file = "{}/.env".format(nb_workdir)
+    ne_trigger_json = json.loads(args.nuvlaedge_trigger_content)
+    nuvlaedge_workdir = args.nuvlaedge_workdir.rstrip('/')
+    nuvlaedge_old_workdir = args.nuvlaedge_old_workdir.rstrip('/')
+    env_file = "{}/.env".format(nuvlaedge_workdir)
+    old_env_file = "{}/.env".format(nuvlaedge_old_workdir)
+
+    installation_strategy = "OVERWRITE" # default
+    nuvlaedge_id = None
+
+    if not os.path.exists(nuvlaedge_workdir):
+        os.makedirs(nuvlaedge_workdir)
+
+    new_conf = ne_trigger_json.get('environment', {})
+    nuvla_endpoint     = ne_trigger_json['endpoint']
+    nuvla_api_endpoint = nuvla_endpoint.rstrip('/').rstrip('/api') + "/api"
+    nuvla_api_key      = ne_trigger_json['apikey']
+    nuvla_api_secret   = ne_trigger_json['apisecret']
+    nuvla_api_insecure = is_true(new_conf.get('NUVLA_ENDPOINT_INSECURE'), default=False)
+    nuvlaedge_release         = ne_trigger_json['version']
+    nuvlaedge_assets          = ne_trigger_json['assets']
+    nuvlaedge_basename        = ne_trigger_json.get('name', '')
+    nuvlaedge_basedescription = ne_trigger_json.get('description', ' ')
+    nuvlaedge_vpn_server_id   = ne_trigger_json.get('vpn')
+    nuvlaedge_ssh             = ne_trigger_json.get('ssh', {})
+    nuvlaedge_ssh_pubkeys = nuvlaedge_ssh.get('public-keys', [])
+    nuvlaedge_version     = nuvlaedge_release.split('.')[0]
+
+    api = nuvla_login(nuvla_api_endpoint, nuvla_api_key, nuvla_api_secret, nuvla_api_insecure)
+
+    new_conf['NUVLA_ENDPOINT'] = nuvla_endpoint
+    new_conf['NUVLA_ENDPOINT_INSECURE'] = str(nuvla_api_insecure)
+    if nuvlaedge_ssh_pubkeys:
+        new_conf['NUVLABOX_SSH_PUB_KEY']  = '\\n'.join(nuvlaedge_ssh_pubkeys)
+        new_conf['NUVLAEDGE_SSH_PUB_KEY'] = '\\n'.join(nuvlaedge_ssh_pubkeys)
 
     # Check if env files already exists
     # cause that will tell us if this is the first time we are self-registring this NB or not
     # if there's a previous env file (thus previous installation), we will check if it is COMMISSIONED or not
     # based on this check, we will either UPDATE or OVERWRITE the existing installation, respectively
-    installation_strategy = "OVERWRITE"    # default
-    nuvlaedge_id = None
+
     previous_conf = {}
-    if not os.path.exists(nb_workdir):
-        os.makedirs(nb_workdir)
-    else:
-        if os.path.isfile(env_file):
-            # .env file exists - get the previous details
-            with open(env_file) as f:
-                for l in f.read().splitlines():
-                    if l and "=" in l:
-                        varname = l.split('=', 1)[0]
-                        varvalue = l.split('=', 1)[1]
-                        previous_conf[varname] = varvalue
-
-    # argparse
-    nuvla = nb_trigger_json['endpoint']
-    nuvla_endpoint = nb_trigger_json['endpoint'].rstrip('/').rstrip('/api') + "/api"
-    nb_basename = nb_trigger_json.get('name', '')
-    nb_basedescription = nb_trigger_json.get('description', ' ')
-    nb_release = nb_trigger_json['version']
-    nb_vpn_server_id = nb_trigger_json.get('vpn')
-    nb_assets = nb_trigger_json['assets']
-    nb_ssh = nb_trigger_json.get('ssh', {})
-    new_conf = nb_trigger_json.get('environment', {})
-
-    nb_ssh_pubkeys = nb_ssh.get('public-keys', [])
-    nb_version = nb_release.split('.')[0]
-
-    login_apikey = {
-        "template": {
-            "href": "session-template/api-key",
-            "key": nb_trigger_json['apikey'],
-            "secret": nb_trigger_json['apisecret']
-        }
-    }
-
-    s = requests.Session()
-
-    # login
-    connection_verify = True
-    login_endpoint = nuvla_endpoint + "/session"
-    print("Nuvla login at {}...".format(login_endpoint))
-    try:
-        session = s.post(login_endpoint, json=login_apikey)
-    except requests.exceptions.SSLError:
-        connection_verify = False
-        session = s.post(login_endpoint, json=login_apikey, verify=connection_verify)
-
-    session.raise_for_status()
-
-    new_conf['NUVLA_ENDPOINT'] = nuvla
-    new_conf['NUVLA_ENDPOINT_INSECURE'] = str(not connection_verify)
-    if nb_ssh_pubkeys:
-        new_conf['NUVLAEDGE_SSH_PUB_KEY'] = '\\n'.join(nb_ssh_pubkeys)
+    if os.path.isfile(env_file):
+        previous_conf.update(read_env_file(env_file))
+    elif os.path.isfile(old_env_file):
+        previous_conf.update(read_env_file(old_env_file))
 
     if previous_conf:
-        if "NUVLAEDGE_UUID" in previous_conf:
-            previous_uuid = previous_conf['NUVLAEDGE_UUID']
+        if "NUVLAEDGE_UUID" in previous_conf or "NUVLABOX_UUID" in previous_conf:
+            previous_uuid = previous_conf.get('NUVLAEDGE_UUID', previous_conf.get("NUVLABOX_UUID"))
             print("Existing env file from previous deployment found, with NuvlaEdge UUID {}".format(previous_uuid))
-            check_nb_endpoint = nuvla_endpoint + "/" + previous_uuid
-            nb = s.get(check_nb_endpoint, verify=connection_verify)
-            if nb.status_code == 200:
-                state = nb.json().get('state', 'UNKNOWN')
+            nuvlaedge_url = nuvla_api_endpoint + "/" + previous_uuid
+            response = api.get(nuvlaedge_url)
+            if response.status_code == 200:
+                nuvlaedge = response.json()
+                state = nuvlaedge.get('state', 'UNKNOWN')
                 if state in ["DECOMMISSIONED", 'ERROR']:
                     # this NuvlaEdge has been decommissioned or is in error, just overwrite the local installation
                     print("Previous NuvlaEdge {} is in state {}. Going to OVERWRITE it...".format(previous_uuid, state))
                 else:
+                    new_conf['NUVLABOX_UUID'] = previous_uuid
                     new_conf['NUVLAEDGE_UUID'] = previous_uuid
                     if new_conf == previous_conf:
                         print("NuvlaEdge environment hasn't changed, performing an UPDATE")
                         installation_strategy = "UPDATE"
                     else:
                         print("NuvlaEdge environment different from existing installation, performing an OVERWRITE")
-            elif nb.status_code == 404:
+            elif response.status_code == 404:
                 # doesn't exist, so let's just OVERWRITE this local installation
                 print("Previous NuvlaEdge {} doesn't exist anymore...creating new one".format(previous_uuid))
             else:
                 # something went wrong, either a network issue or we have the wrong credentials to access the
                 # current NuvlaEdge resource...just throw the error and do nothing
-                nb.raise_for_status()
+                response.raise_for_status()
         else:
             print("There's a previous NuvlaEdge environment but couldn't find a NuvlaEdge UUID...let's OVERWRITE")
 
     if installation_strategy == "OVERWRITE":
         print("Creating new NuvlaEdge resource...")
-        try:
-            unique_id = str(get_mac())
-        except:
-            unique_id = str(int(time.time()))
 
-        nb_name = nb_basename.rstrip("_") + "_" + unique_id if nb_basename else unique_id
-
-        nb_description = "{} - self-registration number {}".format(nb_basedescription, unique_id)
+        nuvlaedge_name, nuvlaedge_description = generate_nuvlaedge_name_description(nuvlaedge_basename, nuvlaedge_basedescription)
 
         nuvlaedge = {
-            "name": nb_name,
-            "description": nb_description,
-            "version": int(nb_version)
+            "name": nuvlaedge_name,
+            "description": nuvlaedge_description,
+            "version": int(nuvlaedge_version)
         }
 
-        if nb_vpn_server_id:
-            nuvlaedge['vpn-server-id'] = nb_vpn_server_id
+        if nuvlaedge_vpn_server_id:
+            nuvlaedge['vpn-server-id'] = nuvlaedge_vpn_server_id
 
-        if nb_ssh and "ids" in nb_ssh and isinstance(nb_ssh.get('ids'), list):
-            nuvlaedge['ssh-keys'] = nb_ssh.get('ids')
+        if nuvlaedge_ssh and "ids" in nuvlaedge_ssh and isinstance(nuvlaedge_ssh.get('ids'), list):
+            nuvlaedge['ssh-keys'] = nuvlaedge_ssh.get('ids')
 
-        new_nb_endpoint = nuvla_endpoint + "/nuvlabox"
-        nb_id = s.post(new_nb_endpoint, json=nuvlaedge, verify=connection_verify)
+        new_nuvlaedge_endpoint = nuvla_api_endpoint + "/nuvlabox"
+        response = api.post(new_nuvlaedge_endpoint, json=nuvlaedge)
 
-        nb_id.raise_for_status()
+        response.raise_for_status()
 
-        nuvlaedge_id = nb_id.json()["resource-id"]
-        print("Created NuvlaEdge resource {} in {}".format(nuvlaedge_id, nuvla))
+        nuvlaedge_id = response.json()["resource-id"]
+        print("Created NuvlaEdge resource {} in {}".format(nuvlaedge_id, nuvla_endpoint))
 
+        new_conf['NUVLABOX_UUID'] = nuvlaedge_id
         new_conf['NUVLAEDGE_UUID'] = nuvlaedge_id
 
     # update env file
@@ -273,21 +345,23 @@ if __name__ == "__main__":
             f.write("{}={}\n".format(varname, varvalue))
 
     try:
-        installer_file, compose_files = prepare_nuvlaedge_engine_installation(nb_release,
-                                                                             nb_assets,
-                                                                             nb_workdir,
-                                                                             keep_files=[env_file])
+        installer_file, compose_files = prepare_nuvlaedge_installation(nuvlaedge_release,
+                                                                              nuvlaedge_assets,
+                                                                              nuvlaedge_workdir,
+                                                                              keep_files=[env_file])
 
-        install_command = ["sh", installer_file, "--env-file={}".format(env_file),
+        install_command = ["sh", installer_file,
+                           "--env-file={}".format(env_file),
                            "--compose-files={}".format(",".join(compose_files)),
-                           "--installation-strategy={}".format(installation_strategy), "--action=INSTALL"]
+                           "--installation-strategy={}".format(installation_strategy),
+                           "--action=INSTALL"]
 
         print("Installing NuvlaEdge Engine - this can take a few minutes...")
-        install_nuvlaedge_engine(install_command)
+        install_nuvlaedge(install_command)
     except:
         # On any error, cleanup the resource in Nuvla
         print("NuvlaEdge Engine installation failed")
         if nuvlaedge_id:
             print("removing {} from Nuvla".format(nuvlaedge_id))
-            s.delete(nuvla_endpoint + "/" + nuvlaedge_id, verify=connection_verify)
+            api.delete(nuvla_api_endpoint + "/" + nuvlaedge_id)
         raise

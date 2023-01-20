5737987445e82245a63c9cc494a4efde76849efb
Mario Trost <mario.trost@bluewin.ch>
2023-01-20 14:37:29 +0100

    Makes app_store work with new tab plugin using query param

--- code/src/cljs/sixsq/nuvla/ui/apps_store/events.cljs
+++ code/src/cljs/sixsq/nuvla/ui/apps_store/events.cljs
@@ -7,7 +7,6 @@
             [sixsq.nuvla.ui.main.spec :as main-spec]
             [sixsq.nuvla.ui.plugins.full-text-search :as full-text-search-plugin]
             [sixsq.nuvla.ui.plugins.pagination :as pagination-plugin]
-            [sixsq.nuvla.ui.plugins.tab-new :as tab-plugin]
             [sixsq.nuvla.ui.session.spec :as session-spec]
             [sixsq.nuvla.ui.utils.general :as general-utils]))
 
@@ -16,8 +15,7 @@
   (fn [{db :db}]
     {:db (merge db spec/defaults)
      :fx [[:dispatch [::apps-events/reset-version]]
-          [:dispatch [::apps-events/module-not-found false]]
-          [:dispatch [::get-modules]]]}))
+          [:dispatch [::apps-events/module-not-found false]]]}))
 
 (reg-event-db
   ::set-modules
@@ -27,8 +25,7 @@
 
 (reg-event-fx
   ::get-modules
-  (fn [{{:keys [::session-spec/session] :as db} :db}]
-    (let [active-tab (keyword (tab-plugin/get-active-tab db [::spec/tab]))]
+  (fn [{{:keys [::session-spec/session] :as db} :db} [_ active-tab]]
       (-> {:db (assoc db ::apps-spec/module nil)
            ::cimi-api-fx/search
            [:module
@@ -46,8 +43,8 @@
                                nil)
                              (full-text-search-plugin/filter-text
                                db [::spec/modules-search]))}
-                 (pagination-plugin/first-last-params db [::spec/pagination]))
-            #(dispatch [::set-modules %])]}))))
+                 (pagination-plugin/first-last-params db [(spec/page-keys->pagination-db-path active-tab)]))
+            #(dispatch [::set-modules %])]})))
 
 (reg-event-fx
   ::get-modules-summary
--- code/src/cljs/sixsq/nuvla/ui/apps_store/spec.cljs
+++ code/src/cljs/sixsq/nuvla/ui/apps_store/spec.cljs
@@ -6,7 +6,9 @@
 
 (s/def ::modules any?)
 (s/def ::tab any?)
-(s/def ::pagination any?)
+(s/def ::pagination-appstore any?)
+(s/def ::pagination-allapps any?)
+(s/def ::pagination-myapps any?)
 (s/def ::modules-search any?)
 
 (def defaults
@@ -14,5 +16,18 @@
    ::tab            (tab-plugin/build-spec :active-tab :appstore)
    ::modules-search (full-text-search-plugin/build-spec)})
 
-(def pagination-default {::pagination (pagination-plugin/build-spec
-                                        :default-items-per-page 8)})
+(def appstore-key :appstore)
+(def allapps-key :allapps)
+(def myapps-key :myapps)
+(def navigate-key :navigate)
+
+(def page-keys->pagination-db-path
+  (zipmap [appstore-key allapps-key myapps-key]
+          [::pagination-appstore ::pagination-allapps ::pagination-myapps]))
+
+(def pagination-default {::pagination-appstore (pagination-plugin/build-spec
+                                                 :default-items-per-page 8)
+                         ::pagination-allapps (pagination-plugin/build-spec
+                                                :default-items-per-page 8)
+                         ::pagination-myapps (pagination-plugin/build-spec
+                                               :default-items-per-page 8)})

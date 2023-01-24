68f947443497170561584d27e04fb0d5dd1d18ab
Mario Trost <mario.trost@bluewin.ch>
2023-01-24 11:45:02 +0100

    Adds visibility e2e check for 2 dropdowns

--- code/test/e2e/loggedin/overlapping_selects.spec.js
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('Language modal not behind main content', async ({ page, context }, { project, config }) => {
-  const { baseURL } = config.projects[0].use;
-  await page.goto(baseURL + '/ui/welcome');
-  await page.pause();
-  await page.locator('#nuvla-ui-main span:has-text("en")').click();
-  await page.getByRole('option', { name: 'English' }).isVisible();
-});
---
+++ code/test/e2e/loggedin/visible_dropdowns.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from '@playwright/test';
+
+test('Language modal not behind main content', async ({ page, context }, { project, config }) => {
+  const { baseURL } = config.projects[0].use;
+  await page.goto(baseURL + '/ui/welcome');
+
+  await page.locator('#nuvla-ui-main span:has-text("en")').click();
+  await page.getByRole('option', { name: 'English' }).isVisible();
+
+  await page.getByRole('link', { name: 'apps' }).click();
+  await page.waitForURL('https://nui.localhost/ui/apps');
+
+  for (let i of [0, 1, 2, 3, 4, 5, 6, 7]) {
+    await page.pause();
+    console.log('testing app no', i);
+    await page
+      .getByText(/(free trial)|(launch)/)
+      .nth(i)
+      .click();
+    await page.locator('a:has-text("Application version")').click();
+    await page
+      .getByText(/^v\d+\s\|/)
+      .nth(0)
+      .click();
+    await page.getByRole('option').nth(1).isVisible();
+    await page.locator('.close').click();
+    await page.getByRole('link', { name: 'apps' }).click();
+    await page.waitForURL('https://nui.localhost/ui/apps');
+  }
+});

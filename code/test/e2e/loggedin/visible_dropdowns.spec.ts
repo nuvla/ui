1eb0587b4ae5a0a58cbd50e3d4427b1c8575f9ce
Mario Trost <mario.trost@bluewin.ch>
2023-01-24 13:58:32 +0100

    Fix failing e2e test

--- code/test/e2e/loggedin/visible_dropdowns.spec.ts
+++ code/test/e2e/loggedin/visible_dropdowns.spec.ts
@@ -7,8 +7,10 @@ test('Language modal not behind main content', async ({ page, context }, { proje
   await page.locator('#nuvla-ui-main span:has-text("en")').click();
   await page.getByRole('option', { name: 'English' }).isVisible();
 
+  const url = `${baseURL}/ui/apps`;
+
   await page.getByRole('link', { name: 'apps' }).click();
-  await page.waitForURL('https://nui.localhost/ui/apps');
+  await page.waitForURL(baseURL);
 
   for (let i of [0, 1, 2, 3, 4, 5, 6, 7]) {
     await page.pause();
@@ -25,6 +27,6 @@ test('Language modal not behind main content', async ({ page, context }, { proje
     await page.getByRole('option').nth(1).isVisible();
     await page.locator('.close').click();
     await page.getByRole('link', { name: 'apps' }).click();
-    await page.waitForURL('https://nui.localhost/ui/apps');
+    await page.waitForURL(baseURL);
   }
 });

04926bb6b9ab783de4bbfffc1d25cbcf7b79dd42
Mario Trost <mario.trost@bluewin.ch>
2023-01-24 14:04:36 +0100

    fixup! Merge branch 'master' into uix

--- code/src/cljs/sixsq/nuvla/ui/edges_detail/views.cljs
+++ code/src/cljs/sixsq/nuvla/ui/edges_detail/views.cljs
@@ -1420,7 +1420,7 @@
             [ui/GridColumn {:stretched true}
              [TabOverviewCluster @nb-status]])
 
-          (when (and (seq tags) (not suspended?))
+          (when-not (and (seq tags) (not suspended?))
             [ui/GridColumn
              [TabOverviewTags @nuvlabox]])
 

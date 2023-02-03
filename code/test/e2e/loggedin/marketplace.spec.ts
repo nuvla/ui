d32fb9666ae3bba76f592b5c8d6d2da3bd359b2c
Mario Trost <mario.trost@bluewin.ch>
2023-02-03 14:13:07 +0100

    Adds e2e test for storing last tab as default

--- code/test/e2e/loggedin/marketplace.spec.ts
+++ code/test/e2e/loggedin/marketplace.spec.ts
@@ -14,9 +14,27 @@ test('Marketplace shows only published apps', async ({ page }, { project, config
   await page.getByRole('link', { name: 'Apps' }).click();
   await expect(page).toHaveURL(marketplaceUrl);
 
-  const appCards = await page.locator('a.ui.card').nth(0).waitFor();
-  const elements = await page.$$('a.ui.card');
+  await page.locator('a.ui.card').nth(0).waitFor();
+  let elements = await page.$$('a.ui.card');
 
   let hrefs = await Promise.all(elements.map(async (elem) => elem.getAttribute('href')));
   expect(hrefs.length).toBeGreaterThan(0);
+  await page.route('/api/module', async (route) => {
+    return route.continue();
+  });
+  await page.pause();
+
+  await elements[0].click();
+  const appdetails = await page.locator('.nuvla-apps');
+  await appdetails.getByRole('link', { name: 'Deployments' }).click();
+  await page.waitForURL(/apps-tab=deployments$/);
+  await page.locator('#nuvla-ui-header-breadcrumb').getByText('Apps').click();
+  await page.waitForURL(`${baseURL}/ui/apps`);
+  await page.locator('a.ui.card').nth(1).click();
+  await page.waitForURL(/apps-tab=deployments$/);
+  expect(page.locator('.nuvla-apps').getByRole('link', { name: 'Deployments' })).toHaveClass(/active/);
+  await page.locator('#nuvla-ui-header-breadcrumb').getByText('Apps').click();
+  await page.waitForURL('https://nui.localhost/ui/apps');
+  await page.getByRole('link', { name: 'Navigate Apps' }).click();
+  await page.waitForURL(`${baseURL}/ui/apps?apps-store-tab=navigate`);
 });

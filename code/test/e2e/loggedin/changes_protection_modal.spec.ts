312060bfb849fe6274d168a23a070348f5f137cd
Mario Trost <mario.trost@bluewin.ch>
2023-02-22 08:05:48 +0100

    Makes e2e test changes protection less flaky

--- code/test/e2e/loggedin/changes_protection_modal.spec.ts
+++ code/test/e2e/loggedin/changes_protection_modal.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test';
 
-test.only('CHANGES PROTECTION MODAL TEST 1a: opens by main navigation, and closes by ignore changes click', async ({
-  page,
-}, { config }) => {
+test.use({ actionTimeout: 5000, navigationTimeout: 5000 });
+
+test('CHANGES PROTECTION MODAL TEST 1: opens by main navigation', async ({ page }, { config }) => {
   const { baseURL } = config.projects[0].use;
   await page.goto(baseURL + '/ui/welcome');
 
@@ -12,43 +12,138 @@ test.only('CHANGES PROTECTION MODAL TEST 1a: opens by main navigation, and close
   await setUp(page);
 
   await page.pause();
-
-  const openModal = () => page.getByRole('link', { name: 'deployments' }).click();
   const url = page.url();
 
-  // clicks X to close it
+  ////////////////////////////////////
+  // TESTING OPEN MODAL BY MAIN NAV //
+  const openModal = () => page.getByRole('link', { name: 'deployments' }).click();
+
+  // CLICKS X for closing
   await openModal();
   await page.locator('.close').click();
-  testModalhidden(page);
-  expect(page.url()).toBe(url);
+  expectModalHidden(page, 'Modal hidden after click on X');
+  testSameUrl(page, url, 'Same URL after click on X');
 
-  // clicks outside modal for closing it
-  // await page.evaluate(() => document.querySelector('[data-testid=protection-modal]')?.parentElement?.click());
+  // NAVIGATE BACK for closing
+  await openModal();
+  await page.goBack();
+  expectModalHidden(page, 'Modal hidden after navigating back');
+  testSameUrl(page, url, 'Same URL after navigating back');
+
+  // CLICKS OUTSIDE MODAL for closing
+  // FIX ME: This test passes in headed and fails in headless mode.
   // await openModal();
+  // next two lines do the same, only use need one
+  // await page.evaluate(() => document.querySelector('[data-testid=protection-modal]')?.parentElement?.click());
   // await page.getByTestId('protection-modal').locator('..').click();
   // expect(page.getByRole('button', { name: 'ignore changes' })).toBeHidden();
   // expect(page.url()).toBe(url);
 
-  await openModal();
-
-  // navigate back for closing modal
-  await page.goBack();
-  testModalhidden(page);
-  expect(page.url()).toBe(url);
+  // //// TEST IGNORE CASE ////
+  // //////////////////////////
 
   // TODO: Test if all protections are still in place
+  // clicks X to close it
+  // Open modal
+  await openModal();
+  await page.locator('.close').click({ timeout: 2000 });
+  expectModalHidden(page, 'Modal still hides after clicking X?');
+  // await page.goBack();
+  // await page.locator('.close').click({ timeout: 2000 });
 
   await openModal();
 
   // ignores changes
   await page.getByRole('button', { name: 'ignore changes' }).click();
-  expect(page.getByRole('button', { name: 'ignore changes' })).toBeHidden({ timeout: 2000 });
-  await page.waitForURL('https://nui.localhost/ui/deployments');
+  expectModalHidden(page, 'Modal hidden after ignoring changes');
+  page.waitForURL(new RegExp('https://nui.localhost/ui/deployments'), { timeout: 2000 });
 });
 
-test('CHANGES PROTECTION MODAL TEST 1b: opens by main navigation, and closes by navigating back', async ({ page }, {
-  config,
-}) => {
+test.skip('CHANGES PROTECTION MODAL TEST 2: opens by navigating back', async ({ page }, { config }) => {
+  const { baseURL } = config.projects[0].use;
+  await page.goto(baseURL + '/ui/welcome');
+
+  //
+  await page.pause();
+  await setUp(page);
+  await page.pause();
+  const url = page.url();
+
+  ////////////////////////////////////
+  // TESTING OPEN MODAL BY MAIN NAV //
+  const openModalByNavBack = () => page.getByRole('link', { name: 'deployments' }).click();
+
+  // CLICKS X for closing
+  await openModalByNavBack();
+  await page.locator('.close').click();
+  expectModalHidden(page, 'Modal hidden after click on X');
+  testSameUrl(page, url, 'Same URL after click on X');
+
+  // NAVIGATE BACK for closing
+  await openModalByNavBack();
+  await page.goBack();
+  expectModalHidden(page, 'Modal hidden after navigating back');
+  testSameUrl(page, url, 'Same URL after navigating back');
+
+  // CLICKS OUTSIDE MODAL for closing
+  // FIX ME: This test passes in headed and fails in headless mode.
+  // await openModal();
+  // next two lines do the same, only use need one
+  // await page.evaluate(() => document.querySelector('[data-testid=protection-modal]')?.parentElement?.click());
+  // await page.getByTestId('protection-modal').locator('..').click();
+  // expect(page.getByRole('button', { name: 'ignore changes' })).toBeHidden();
+  // expect(page.url()).toBe(url);
+
+  // ////////////////////////////////////
+  // // TESTING OPEN MODAL BY MAIN NAV //
+  // const openModalByBrowserBack = () => page.goBack();
+
+  // // CLICKS X for closing
+  // await openModalByBrowserBack();
+  // await page.locator('.close').click({ timeout: 2000 });
+  // testModalhidden(page);
+  // expect(page.url()).toBe(url);
+
+  // // NAVIGATE BACK for closing
+  // await openModalByBrowserBack();
+  // await page.goBack();
+  // testModalhidden(page);
+  // expect(page.url()).toBe(url);
+
+  // // CLICKS OUTSIDE MODAL for closing
+  // // FIX ME: This test passes in headed and fails in headless mode.
+  // // await openModalByBrowserBack();
+  // // next two lines do the same, only use need one
+  // // await page.evaluate(() => document.querySelector('[data-testid=protection-modal]')?.parentElement?.click());
+  // // await page.getByTestId('protection-modal').locator('..').click();
+  // // expect(page.getByRole('button', { name: 'ignore changes' })).toBeHidden();
+  // // expect(page.url()).toBe(url);
+
+  // //////////////////////////
+  // //////////////////////////
+  // //////////////////////////
+  // //////////////////////////
+  // //// TEST IGNORE CASE ////
+  // //////////////////////////
+  await openModalByNavBack();
+
+  // TODO: Test if all protections are still in place
+  // clicks X to close it
+  // Open modal
+  expectModalVisible(page);
+  await page.locator('.close').click();
+
+  await openModalByNavBack();
+
+  // ignores changes
+  await page.getByRole('button', { name: 'ignore changes' }).click();
+  expectModalHidden(page, 'Modal hidden after ignoring changes', 2000);
+  page.waitForURL(new RegExp('https://nui.localhost/ui/deployments'), { timeout: 2000 });
+});
+
+test.skip('CHANGES PROTECTION MODAL TEST 1b: opens by main navigation, and closes by navigating back', async ({
+  page,
+}, { config }) => {
   const { baseURL } = config.projects[0].use;
   await page.goto(baseURL + '/ui/welcome');
 
@@ -107,8 +202,16 @@ test.skip('CHANGES PROTECTION MODAL TEST 3: opens by navigating back, 2) and clo
   await page.waitForURL('https://nui.localhost/ui/apps/do-not-delete--e2e-test-project?apps-project-tab=overview');
 });
 
-function testModalhidden(page) {
-  expect(page.getByRole('button', { name: 'ignore changes' })).toBeHidden();
+function testSameUrl(page, url: string, errorMessage = 'Same URL?') {
+  expect(page.url(), errorMessage).toBe(url);
+}
+
+function expectModalVisible(page, errorMessage = 'Modal visible?') {
+  expect(page.getByRole('button', { name: 'ignore changes' }), errorMessage).toBeVisible({ timeout: 2000 });
+}
+
+function expectModalHidden(page, errorMessage = 'Modal hidden?', timeout = 0) {
+  expect(page.getByRole('button', { name: 'ignore changes' }), errorMessage).toBeHidden({ timeout });
 }
 
 async function setUp(page) {

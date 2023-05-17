ce5eb96bdbc423414051b8cf7b0cba8fb9dedc3a
khaled basbous <khaled@sixsq.com>
2023-05-17 09:16:03 +0200

    e2e tests - apps versions use base-uri

--- code/test/e2e/loggedin/apps_versions.spec.ts
+++ code/test/e2e/loggedin/apps_versions.spec.ts
@@ -10,21 +10,21 @@ test('Changing apps version does not affect other app', async ({ page }, { proje
 
   await page.getByPlaceholder('Search...').click();
   await page.getByPlaceholder('Search...').fill('latence');
-  await page.waitForURL('https://nui.localhost/ui/apps?apps-store-modules-search=latence');
+  await page.waitForURL(`${baseURL}/ui/apps?apps-store-modules-search=latence`);
   await page
     .getByRole('link', {
       name: 'LatenceTech Analyzer Collects, stores and performs real-time analysis on the measurement results (sent by the QoSAgent). Offers real-time observability using ready-to-use dashboards to view results and… Real-time Dashboards Time-Series Database Latency Analytics free trial and then €8.30/day',
     })
     .click();
-  await page.waitForURL('https://nui.localhost/ui/apps/latencetech/analyzer/?version=8&apps-tab=overview');
+  await page.waitForURL(`${baseURL}/ui/apps/latencetech/analyzer/?version=8&apps-tab=overview`);
   await page.getByRole('link', { name: 'Versions' }).click();
-  await page.waitForURL('https://nui.localhost/ui/apps/latencetech/analyzer/?version=8&apps-tab=versions');
+  await page.waitForURL(`${baseURL}/ui/apps/latencetech/analyzer/?version=8&apps-tab=versions`);
   await page.locator('a:has-text("v7")').click();
   await page.locator('#nuvla-ui-header-breadcrumb').getByText('latencetech').click();
-  await page.waitForURL('https://nui.localhost/ui/apps/latencetech?apps-project-tab=overview');
+  await page.waitForURL(`${baseURL}/ui/apps/latencetech?apps-project-tab=overview`);
   await page.getByText('LatenceTech QoSAgent').click();
   await page.getByRole('link', { name: 'Versions' }).click();
-  await page.waitForURL('https://nui.localhost/ui/apps/latencetech/latencetech?apps-tab=versions');
+  await page.waitForURL(`${baseURL}/ui/apps/latencetech/latencetech?apps-tab=versions`);
   await page.getByRole('cell', { name: 'v7' }).isVisible({ timeout: 2000 });
   await page.getByRole('cell', { name: 'v7 <<' }).isHidden({ timeout: 2000 });
 });

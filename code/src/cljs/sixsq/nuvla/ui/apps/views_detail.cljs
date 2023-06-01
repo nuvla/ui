477b99acae0169582b71c12e9c94a3ddfdce40a9
Mario Trost <mario.trost@bluewin.ch>
2023-06-01 10:31:56 +0200

    App details tab content design changes

--- code/src/cljs/sixsq/nuvla/ui/apps/views_detail.cljs
+++ code/src/cljs/sixsq/nuvla/ui/apps/views_detail.cljs
@@ -1130,7 +1130,7 @@
             {:keys [license-name]} @license]
         [:<>
          (when (not= utils/subtype-component subtype)
-           [:h2 [LicenseTitle {:full true}]])
+           [:h4 {:class :tab-app-detail} [LicenseTitle {:full true}]])
          (if (or @editable? (some? @license))
            [ui/Form
             (when @editable?
--- code/src/cljs/sixsq/nuvla/ui/apps_application/views.cljs
+++ code/src/cljs/sixsq/nuvla/ui/apps_application/views.cljs
@@ -313,8 +313,8 @@
   []
   (let [tr      (subscribe [::i18n-subs/tr])
         is-new? (subscribe [::apps-subs/is-new?])]
-    [:div {:class :uix-apps-details-deployments}
-     [:h2 [apps-views-detail/DeploymentsTitle]]
+    [:div {:class :uix-apps-details-details}
+     [:h4 {:class :tab-app-detail} [apps-views-detail/DeploymentsTitle]]
      (if @is-new?
        [uix/WarningMsgNoElements]
        [deployments-views/DeploymentTable
@@ -345,8 +345,8 @@
 
 (defn ConfigurationPane
   []
-  [:div {:class :uix-apps-details-configuration}
-   [:h2 [apps-views-detail/ConfigurationTitle]]
+  [:div {:class :uix-apps-details-details}
+   [:h4 {:class :tab-app-detail} [apps-views-detail/ConfigurationTitle]]
    [apps-views-detail/EnvVariablesSection]
    [FilesSection]
    [apps-views-detail/UrlsSection]
@@ -363,7 +363,8 @@
 
 (defn LicensePane
   []
-  [apps-views-detail/LicenseSection])
+  [:div {:class :uix-apps-details-details}
+   [apps-views-detail/LicenseSection]])
 
 
 (defn TabMenuPricing
@@ -379,8 +380,9 @@
         price     (subscribe [::apps-subs/price])
         vendor    (subscribe [::profile-subs/vendor])]
     (fn []
-      [:div {:class :uix-apps-details-pricing}
-       [:h2 [apps-views-detail/PricingTitle]]
+      [:div {:class :uix-apps-details-details}
+       [:h4  {:class :tab-app-detail}
+        [apps-views-detail/PricingTitle]]
        (if (or (and @editable? @vendor) (some? @price))
          [apps-views-detail/Pricing]
          [:<>
@@ -405,8 +407,8 @@
   (let [active-tab     (sub-apps-tab)
         module-subtype (subscribe [::apps-subs/module-subtype])]
     @active-tab
-    [:div {:class :uix-apps-details-docker}
-     [:h2 [apps-views-detail/DockerTitle]]
+    [:div {:class :uix-apps-details-details}
+     [:h4 {:class :tab-app-detail} [apps-views-detail/DockerTitle]]
      [apps-views-detail/registries-section]
      ^{:key (random-uuid)}
      [:div

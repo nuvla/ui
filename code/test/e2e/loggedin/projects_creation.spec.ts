5b531bc56055614f59125e365390a7c24142f616
Mario Trost <mario.trost@bluewin.ch>
2023-06-27 18:11:28 +0200

    Dissoc content from new project

---
+++ code/test/e2e/loggedin/projects_creation.spec.ts
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test';
+
+test.use({ navigationTimeout: 5000, actionTimeout: 5000 });
+
+test('Creating a new project', async ({ page }, { config }) => {
+  const { baseURL } = config.projects[0].use;
+  await page.goto(baseURL + '/ui/welcome');
+
+  await page.getByRole('link', { name: 'Apps' }).nth(0).click();
+  await expect(page).toHaveURL(new RegExp(`${baseURL}/ui/apps`));
+
+  await page.getByText('Navigate Apps').nth(0).click();
+  await expect(page).toHaveURL(`${baseURL}/ui/apps?apps-store-tab=navigate`);
+  await page.locator('a:has-text("Add")').click();
+  await page.getByRole('link', { name: 'Project' }).nth(1).click();
+  await expect(page).toHaveURL('https://nui.localhost/ui/apps/New%20Project?subtype=project&apps-project-tab=details');
+  await page.locator('input[type="input"]').click();
+  await page.locator('input[type="input"]').fill('ThisIsJustATest');
+  await page
+    .getByText('# Project Description PlaceholderThis is a generic placeholder that you should r')
+    .fill('TESTING');
+  await page.pause();
+  await page.locator('a:has-text("Save")').click();
+  await page.getByRole('button', { name: 'save' }).click();
+  await expect(page).toHaveURL('https://nui.localhost/ui/apps/thisisjustatest?apps-project-tab=overview');
+  await page.locator('a:has-text("Delete")').click();
+  await page.getByText('I understand that deleting this application is permanent and cannot be undone.').click();
+  await page.getByRole('button', { name: 'Delete Application' }).click();
+  await page.getByRole('button', { name: 'Yes: Delete Application' }).click();
+  await expect(page).toHaveURL('https://nui.localhost/ui/apps?apps-store-tab=appstore');
+});

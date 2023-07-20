752e95a3dc4bb451afe3834b1c3fdb3e3efd5a51
MTrost <mario@sixsq.com>
2023-07-20 11:58:32 +0200

    Api input field bug fix: Encode URI component (#1341)
    * Encode URI component
    
    * do not encode nil value bugfix
    
    ---------
    
    Co-authored-by: khaled basbous <khaled@sixsq.com>

--- code/src/cljs/sixsq/nuvla/ui/routing/events.cljs
+++ code/src/cljs/sixsq/nuvla/ui/routing/events.cljs
@@ -5,6 +5,7 @@
             [sixsq.nuvla.ui.main.spec :as main-spec]
             [sixsq.nuvla.ui.routing.effects :as fx]
             [sixsq.nuvla.ui.routing.utils :as utils]
+            [sixsq.nuvla.ui.utils.helpers :refer [update-values]]
             [taoensso.timbre :as log]))
 
 (def after-nav-cb-key ::after-nav-cb)
@@ -102,6 +103,8 @@
                         {:change-event            change-event
                          :ignore-chng-protection? ignore-chng-protection?}]]]})))
 
+(defn keyword->string [k] (if (keyword? k) (name k) k))
+
 (reg-event-fx
   ::change-query-param
   (fn [{{:keys [current-route] :as db} :db} [_ new-partial-route-data]]
@@ -110,7 +113,13 @@
                   query-params]} (utils/new-route-data current-route new-partial-route-data)
           push-state?            (:push-state? new-partial-route-data)]
       {:db (assoc db ::ignore-changes-protection true)
-       :fx [[(if push-state? ::fx/push-state ::fx/replace-state) (utils/name->href route-name path-params query-params)]]})))
+       :fx [[(if push-state? ::fx/push-state ::fx/replace-state)
+             (utils/name->href
+               route-name
+               path-params
+               (update-values query-params #(some-> %
+                                                    keyword->string
+                                                    js/encodeURIComponent)))]]})))
 
 (reg-event-fx
   ::store-in-query-param
--- code/test/e2e/loggedin/api.spec.js
+++ code/test/e2e/loggedin/api.spec.js
@@ -7,32 +7,29 @@ test('Navigation api pages and title check', async ({ page }, { project, config 
 
   await page.getByRole('link', { name: 'Api' }).click();
   await expect(page).toHaveURL(apiUrl);
-  let collections = ['user', 'session', 'user-template']
-    for (const collection of collections) {
-      const urlToVisit = apiUrl + '/' + collection;
-      await page.getByRole('combobox', { name: 'resource type' }).locator('i').click();
-      await page.getByRole('option', { name: collection }).click();
-      await expect(page).toHaveURL(urlToVisit);
-      await expect(page).toHaveTitle('Nuvla api/' + collection)
-    }
-    await page.waitForTimeout(300);
-    expect(await page.getByRole('row').count()).toBeGreaterThan(1);
-    await page.getByRole('cell', { name: 'id' }).locator('a').nth(1).click();
-    await expect(page.getByPlaceholder('e.g. created:desc, ...')).toHaveValue('id:asc');
-    await page.getByRole('cell', { name: 'id' }).locator('a').nth(1).click();
-    await expect(page.getByPlaceholder('e.g. created:desc, ...')).toHaveValue('id:desc');
-    await page.getByRole('cell', { name: 'id' }).locator('a').nth(1).click();
-    console.info(await page.getByPlaceholder('e.g. created:desc, ...').inputValue());
-    await expect(page.getByPlaceholder('e.g. created:desc, ...')).toHaveValue('');
-    await page.getByRole('cell', { name: 'name' }).locator('a').nth(1).click();
-    await expect(page.getByPlaceholder('e.g. created:desc, ...')).toHaveValue('name:asc');
-    await expect(page).toHaveURL(
-      apiUrl + '/user-template?first=0&last=20&filter=&orderby=name:asc&select=&aggregation='
-      );
-    await page.getByRole('cell', { name: 'id' }).locator('a').nth(1).click();
-    await expect(page.getByPlaceholder('e.g. created:desc, ...')).toHaveValue('id:asc');
-    await expect(page).toHaveURL(
-      apiUrl + '/user-template?first=0&last=20&filter=&orderby=id:asc&select=&aggregation='
-      );
-
+  let collections = ['user', 'session', 'user-template'];
+  for (const collection of collections) {
+    const urlToVisit = apiUrl + '/' + collection;
+    await page.getByRole('combobox', { name: 'resource type' }).locator('i').click();
+    await page.getByRole('option', { name: collection }).click();
+    await expect(page).toHaveURL(urlToVisit);
+    await expect(page).toHaveTitle('Nuvla api/' + collection);
+  }
+  await page.waitForTimeout(300);
+  expect(await page.getByRole('row').count()).toBeGreaterThan(1);
+  await page.getByRole('cell', { name: 'id' }).locator('a').nth(1).click();
+  await expect(page.getByPlaceholder('e.g. created:desc, ...')).toHaveValue('id:asc');
+  await page.getByRole('cell', { name: 'id' }).locator('a').nth(1).click();
+  await expect(page.getByPlaceholder('e.g. created:desc, ...')).toHaveValue('id:desc');
+  await page.getByRole('cell', { name: 'id' }).locator('a').nth(1).click();
+  console.info(await page.getByPlaceholder('e.g. created:desc, ...').inputValue());
+  await expect(page.getByPlaceholder('e.g. created:desc, ...')).toHaveValue('');
+  await page.getByRole('cell', { name: 'name' }).locator('a').nth(1).click();
+  await expect(page.getByPlaceholder('e.g. created:desc, ...')).toHaveValue('name:asc');
+  await expect(page).toHaveURL(
+    apiUrl + '/user-template?first=0&last=20&filter=&orderby=name%3Aasc&select=&aggregation='
+  );
+  await page.getByRole('cell', { name: 'id' }).locator('a').nth(1).click();
+  await expect(page.getByPlaceholder('e.g. created:desc, ...')).toHaveValue('id:asc');
+  await expect(page).toHaveURL(apiUrl + '/user-template?first=0&last=20&filter=&orderby=id%3Aasc&select=&aggregation=');
 });

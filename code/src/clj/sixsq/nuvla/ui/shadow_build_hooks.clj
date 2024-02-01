15913103766a5fcc3ac84ff7eeedeafcb2957f9e
Khaled Basbous <khaled@sixsq.com>
2024-02-01 14:56:48 +0100

    build version with github bugfix

--- code/src/clj/sixsq/nuvla/ui/shadow_build_hooks.clj
+++ code/src/clj/sixsq/nuvla/ui/shadow_build_hooks.clj
@@ -20,8 +20,8 @@
 (defn- git-commit-short-id
   []
   (or
-    (when-let [github-sha (System/getenv "GITHUB_SHA")]
-      (git-rev-parse (str github-sha "^")))
+    (when-let [github-pr-sha (System/getenv "GITHUB_PR_COMMIT_SHA")]
+      (git-rev-parse github-pr-sha))
     (when-let [cloudflare-sha (System/getenv "CF_PAGES_COMMIT_SHA")]
       (subs cloudflare-sha 0 8))
     (git-rev-parse "HEAD")

e9d8aec894e495334ddd10d58489b452665213fc
khaled basbous <khaled@sixsq.com>
2024-02-01 13:09:03 +0100

    github debug

--- code/src/clj/sixsq/nuvla/ui/shadow_build_hooks.clj
+++ code/src/clj/sixsq/nuvla/ui/shadow_build_hooks.clj
@@ -19,6 +19,9 @@
 
 (defn- git-commit-short-id
   []
+  (prn "printenv" (:out (sh/sh "printenv")))
+  (prn "GITHUB_SHA=" (System/getenv "GITHUB_SHA"))
+  (prn "git log" (:out (sh/sh "git" "--no-pager" "log" "-n" "4")))
   (or
     (when-let [github-sha (System/getenv "GITHUB_SHA")]
       (git-rev-parse (str github-sha "^")))

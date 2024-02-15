341512418a652d6a1662492876db871c2dedeb3f
pirp <kleopatra.pirpinia@gmail.com>
2024-02-15 09:28:35 +0100

    Do not extract graph options fn, test if graph is clickable

--- code/src/cljs/sixsq/nuvla/ui/edges/views_timeseries.cljs
+++ code/src/cljs/sixsq/nuvla/ui/edges/views_timeseries.cljs
@@ -1,6 +1,7 @@
 (ns sixsq.nuvla.ui.edges.views-timeseries
   (:require
     [re-frame.core :refer [dispatch subscribe]]
+    [reagent.core :as r]
     [sixsq.nuvla.ui.edges.subs :as subs]
     [sixsq.nuvla.ui.utils.general :as general-utils]
     [sixsq.nuvla.ui.utils.plot :as plot]
@@ -11,54 +12,62 @@
     [sixsq.nuvla.ui.utils.timeseries :as ts-utils]
     [sixsq.nuvla.ui.utils.ui-callback :as ui-callback]))
 
-(defn graph-options [timespan {:keys [title y-config plugins x-config]}]
-  (let [[from to] (ts-utils/timespan-to-period timespan)]
-    {:plugins  (merge {:title {:display  true
-                               :text     title
-                               :position "top"}}
-                      plugins)
-     :elements {:point {:radius 1}}
-     :scales   {:x (merge {:type  "time"
-                                   :min   from
-                                   :max   to
-                                   :time  {:unit (case timespan
-                                                   ("last 15 minutes"
-                                                     "last hour"
-                                                     "last 12 hours") "minute"
-                                                   "last day" "hour"
-                                                   "last year" "month"
-                                                   "day")}
-                                   :title {:display "true"
-                                           :text    "Time"}}
-                          x-config)
-                :y y-config}}))
-
 (defn timestamp+value [data value-key]
   (mapv (fn [d]
           (assoc d :x (:timestamp d)
                    :y (get-in d [:aggregations value-key :value]))) data))
 
 (defn FleetStatusTimeSeries [timespan data]
-  (let [ts-data (ts-utils/data->ts-data data)]
-    [plot/Bar {:data    {:datasets [{:data            (timestamp+value ts-data :virtual-edges-online)
-                                     :label           "online"
-                                     :backgroundColor "#21d32c88"}
-                                    {:data            (mapv (fn [d]
-                                                              (let [offline-edges (get-in d [:aggregations :virtual-edges-offline :value])
-                                                                    unknown-edges (get-in d [:aggregations :virtual-edges-unknown-state :value])]
-                                                                (assoc d :x (:timestamp d)
-                                                                         :y (+ offline-edges unknown-edges)))) ts-data)
-                                     :label           "unknown or offline"
-                                     :backgroundColor "#eab81198"}]}
+  (r/with-let [ extra-info-visible? (r/atom false)]
 
-               :options (graph-options timespan {:title    "Fleet status"
-                                                 :x-config {:stacked true}
-                                                 :y-config {:max     (get-in data [:dimensions :nuvlaedge-count])
-                                                            :min     0
-                                                            :title   {:display "true"
-                                                                      :text    "Number of NuvlaEdges"}
-                                                            :stacked true
-                                                            }})}]))
+    (let [ts-data             (ts-utils/data->ts-data data)
+          [from to] (ts-utils/timespan-to-period timespan)]
+      [:div {:style {:display "flex"}}
+       [plot/Bar {:data    {:datasets [{:data            (timestamp+value ts-data :virtual-edges-online)
+                                        :label           "online"
+                                        :backgroundColor "#21d32c88"}
+                                       {:data            (mapv (fn [d]
+                                                                 (let [offline-edges (get-in d [:aggregations :virtual-edges-offline :value])
+                                                                       unknown-edges (get-in d [:aggregations :virtual-edges-unknown-state :value])]
+                                                                   (assoc d :x (:timestamp d)
+                                                                            :y (+ offline-edges unknown-edges)))) ts-data)
+                                        :label           "unknown or offline"
+                                        :backgroundColor "#eab81198"}]}
+
+                  :options {:title    "Fleet status"
+                            :scales   {:x {:type    "time"
+                                           :min     from
+                                           :max     to
+                                           :time    {:unit (case timespan
+                                                             ("last 15 minutes"
+                                                               "last hour"
+                                                               "last 12 hours") "minute"
+                                                             "last day" "hour"
+                                                             "last year" "month"
+                                                             "day")}
+                                           :title   {:display "true"
+                                                     :text    "Time"}
+                                           :stacked true}
+                                       :y {:max     (get-in data [:dimensions :nuvlaedge-count])
+                                           :min     0
+                                           :title   {:display "true"
+                                                     :text    "Number of NuvlaEdges"}
+                                           :stacked true
+                                           }}
+                            :elements {:point {:radius 1}}
+                            :onClick  (fn [_evt element _chart]
+                                        (when-let [raw-data (.. (first element) -element -$context -raw)]
+                                          (js/console.log raw-data)
+                                          #_(reset! element-data (js->clj raw-data :keywordize-keys true))
+                                          (swap! extra-info-visible? not)))
+                            :onHover (fn [evt chartElement]
+                                           (let [cursor (if (first chartElement)
+                                                          "pointer"
+                                                          "default")]
+                                             (set! (.. evt -native -target -style -cursor) cursor)))
+                            }}]
+       (when @extra-info-visible?
+         [:div "hello"])])))
 
 (defn FleetTimeSeries []
   (let [tr               (subscribe [::i18n-subs/tr])

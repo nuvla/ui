07b03d1fa7e4d024d4476bf6344bd97685b1f041
pirp <kleopatra.pirpinia@gmail.com>
2024-05-29 10:47:30 +0200

    Revert changes to db->module fn

--- code/src/cljs/sixsq/nuvla/ui/pages/apps/apps_application/utils.cljs
+++ code/src/cljs/sixsq/nuvla/ui/pages/apps/apps_application/utils.cljs
@@ -48,18 +48,8 @@
         docker-compose                (get-in db [::spec/module-application ::spec/docker-compose])
         compatibility                 (get-in db [::spec/module-application ::spec/compatibility])
         files                         (files->module db)
-        requires-user-rights          (get-in db [::spec/module-application ::spec/requires-user-rights])
-        helm-info                     (::apps-spec/helm-info db)
-        helm-absolute-url-provided?   (:helm-absolute-url helm-info)]
+        requires-user-rights          (get-in db [::spec/module-application ::spec/requires-user-rights])]
     (as-> module m
-          (if (and (= subtype "application_helm") helm-absolute-url-provided?)
-            (assoc-in m [:content :helm-absolute-url] (:helm-absolute-url helm-info))
-            m)
-          (if (and (= subtype "application_helm") (not helm-absolute-url-provided?))
-            (-> m
-                (assoc-in [:content :helm-repo-url] (:helm-repo-url helm-info))
-                (assoc-in [:content :helm-chart-name] (:helm-chart-name helm-info)))
-            m)
           (assoc-in m [:content :author] author)
           (assoc-in m [:content :commit] (if (empty? commit) "no commit message" commit))
           (if docker-compose

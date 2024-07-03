5ff29be1c4f6a521d3e28b21219d9488559f23a3
khaled basbous <khaled@sixsq.com>
2024-07-03 18:36:42 +0300

    format bytes for cells

--- code/src/cljs/sixsq/nuvla/ui/pages/data_set/utils.cljs
+++ code/src/cljs/sixsq/nuvla/ui/pages/data_set/utils.cljs
@@ -11,18 +11,6 @@
        (time/time->utc-str time-end)
        "')"))
 
-(defn format-bytes
-  [bytes]
-  (if (number? bytes)
-    (let [scale 1024
-          units ["B" "KiB" "MiB" "GiB" "TiB" "PiB" "EiB"]]
-      (if (< bytes scale)
-        (str bytes " B")
-        (let [exp    (int (/ (js/Math.log bytes) (js/Math.log scale)))
-              prefix (get units exp)
-              v      (/ bytes (js/Math.pow scale exp))]
-          (str (general-utils/round-up v :n-decimal 1) prefix))))
-    "..."))
 
 (defn data-record-geometry-filter
   [geo-operation data-record-map-geojson]

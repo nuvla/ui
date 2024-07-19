30a9a0efee2b58077d1c9ecae6c5ce8de5b16217
Lionel <schaubl@users.noreply.github.com>
2024-07-19 15:22:57 +0200

    feat(apps): allow files for compose apps (#1593)

--- code/src/cljs/sixsq/nuvla/ui/pages/apps/apps_application/views.cljs
+++ code/src/cljs/sixsq/nuvla/ui/pages/apps/apps_application/views.cljs
@@ -83,36 +83,32 @@
 
 
 (defn FilesSection []
-  (let [tr            (subscribe [::i18n-subs/tr])
-        files         (subscribe [::subs/files])
-        editable?     (subscribe [::apps-subs/editable?])
-        compatibility (subscribe [::subs/compatibility])]
+  (let [tr        (subscribe [::i18n-subs/tr])
+        files     (subscribe [::subs/files])
+        editable? (subscribe [::apps-subs/editable?])]
     (fn []
       [uix/Accordion
-       (if (not= @compatibility "docker-compose")
-         [:<>
-          [:div (@tr [:module-files])
-           [uix/HelpPopup (@tr [:module-files-help])]]
+       [:<>
+        [:div (@tr [:module-files])
+         [uix/HelpPopup (@tr [:module-files-help])]]
 
-          (if (empty? @files)
-            [ui/Message
-             (str/capitalize (str (@tr [:no-files]) "."))]
-            [:div [ui/Table {:style {:margin-top 10}}
-                   [ui/TableHeader
-                    [ui/TableRow
-                     [ui/TableHeaderCell {:content (str/capitalize (@tr [:filename]))}]
-                     [ui/TableHeaderCell {:content (str/capitalize (@tr [:content]))}]
-                     (when @editable?
-                       [ui/TableHeaderCell {:content (str/capitalize (@tr [:action]))}])]]
-                   [ui/TableBody
-                    (for [[id file] @files]
-                      ^{:key (str "file_" id)}
-                      [SingleFile file])]]])
-          (when @editable?
-            [:div {:style {:padding-top 10}}
-             [apps-views-detail/plus ::events/add-file]])]
-         [:div (@tr [:apps-file-config-warning])
-          [:a {:href docker-docu-link} (str " " (@tr [:apps-file-config-warning-options-link]))]])
+        (if (empty? @files)
+          [ui/Message
+           (str/capitalize (str (@tr [:no-files]) "."))]
+          [:div [ui/Table {:style {:margin-top 10}}
+                 [ui/TableHeader
+                  [ui/TableRow
+                   [ui/TableHeaderCell {:content (str/capitalize (@tr [:filename]))}]
+                   [ui/TableHeaderCell {:content (str/capitalize (@tr [:content]))}]
+                   (when @editable?
+                     [ui/TableHeaderCell {:content (str/capitalize (@tr [:action]))}])]]
+                 [ui/TableBody
+                  (for [[id file] @files]
+                    ^{:key (str "file_" id)}
+                    [SingleFile file])]]])
+        (when @editable?
+          [:div {:style {:padding-top 10}}
+           [apps-views-detail/plus ::events/add-file]])]
        :label (@tr [:module-files])
        :count (count @files)
        :default-open true])))
@@ -254,7 +250,7 @@
      [:h4 {:class "tab-app-detail"} (str/capitalize (@tr [:summary]))]
      [ui/Table {:basic  "very"
                 :padded false
-                :fixed true}
+                :fixed  true}
       [ui/TableBody
        (when name
          [ui/TableRow
@@ -421,9 +417,9 @@
 
 
 (defn DetailsPane []
-  (let [tr             (subscribe [::i18n-subs/tr])
-        active-tab     (sub-apps-tab)
-        editable?   (subscribe [::apps-subs/editable?])]
+  (let [tr         (subscribe [::i18n-subs/tr])
+        active-tab (sub-apps-tab)
+        editable?  (subscribe [::apps-subs/editable?])]
     @active-tab
     ^{:key (random-uuid)}
     [apps-views-detail/Details

1ccdf7527153aaa66dadd32899cc4fcce9e82003
Alessandro Bellucci <alessandro.bellucci@sixsq.com>
2024-07-30 09:44:02 +0200

    ci(ci): Reuse GitHub workflow code between dev and release (#1610)

--- src/main/resources/versions.sh
+++
@@ -1,2 +0,0 @@
-TAG_VERSION=${parsedVersion.majorVersion}.${parsedVersion.minorVersion}.${parsedVersion.incrementalVersion}
-NEXT_VERSION=${parsedVersion.majorVersion}.${parsedVersion.minorVersion}.${parsedVersion.nextIncrementalVersion}-SNAPSHOT

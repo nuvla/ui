d6898c865a1c7f1e108b2653f9642f859d819e3c
Alessandro Bellucci <alessandro.bellucci@sixsq.com>
2024-09-23 10:53:07 +0200

    feat(portfolio): integrate with the Portfolio library

--- code/test/component/table.spec.ts
+++ code/test/component/table.spec.ts
@@ -5,11 +5,11 @@ test('test', async ({ page }, { config }) => {
 
   await page.goto(baseURL + '/?id=sixsq.nuvla.ui.components.table-scenes%2Fsimple-table');
 
+  const iframe = await page.frameLocator('iframe.canvas');
+
   console.log("page content:" + await page.content());
 
-  const iframe = page.frameLocator('iframe.canvas');
-
-  const table = iframe.first().locator('table.ui');
+  const table = await iframe.first().locator('table.ui');
   expect(table).toHaveCount(1);
 
   expect(table.first().locator('tr')).toHaveCount(2);

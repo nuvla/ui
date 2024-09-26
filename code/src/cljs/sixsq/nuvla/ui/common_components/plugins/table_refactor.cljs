6d640a37373278f2e54323560645730881eead19
khaled basbous <khaled@sixsq.com>
2024-09-26 11:06:51 +0200

    test columns remove and add

--- code/src/cljs/sixsq/nuvla/ui/common_components/plugins/table_refactor.cljs
+++ code/src/cljs/sixsq/nuvla/ui/common_components/plugins/table_refactor.cljs
@@ -25,10 +25,12 @@
   (let [visible-columns @(!visible-columns-fn control)]
     (when (and (> (count visible-columns) 1) (not no-delete))
       [:span {:style {:margin-left "0.8rem"}}
-       [uix/LinkIcon {:color    "red"
-                      :name     "remove circle"
-                      :on-click #(set-current-columns-fn (remove-field-key visible-columns field-key))
-                      :class    :toggle-invisible-on-parent-hover}]])))
+       [uix/LinkIcon {:data-testid "DeleteColumn"
+                      :aria-label  "Delete Column"
+                      :color       "red"
+                      :name        "remove circle"
+                      :on-click    #(set-current-columns-fn (remove-field-key visible-columns field-key))
+                      :class       :toggle-invisible-on-parent-hover}]])))
 
 (defn TableHeaderCell
   [control column]
@@ -168,75 +170,75 @@
                                  ::!data                  (r/atom [{:RepoDigests
                                                                     ["nuvladev/nuvlaedge@sha256:56f8fe1fdf35d50577ab135dcbf78cfb877ccdc41948ec9352d526614b7462f2"],
                                                                     :Labels
-                                                                    {:git.build.time "$(date --utc +%FT%T.%3NZ)",
-                                                                     :org.opencontainers.image.vendor "SixSq SA",
+                                                                    {:git.build.time                   "$(date --utc +%FT%T.%3NZ)",
+                                                                     :org.opencontainers.image.vendor  "SixSq SA",
                                                                      :org.opencontainers.image.created "$(date --utc +%FT%T.%3NZ)",
-                                                                     :git.run.id "10816164086",
+                                                                     :git.run.id                       "10816164086",
                                                                      :org.opencontainers.image.url
                                                                      "https://github.com/nuvlaedge/nuvlaedge",
                                                                      :org.opencontainers.image.authors "support@sixsq.com",
-                                                                     :git.branch "coe-resources",
-                                                                     :git.commit.id "24bb0659461896b22a4a8b675a30b011bbf4efe4",
-                                                                     :org.opencontainers.image.title "NuvlaEdge",
+                                                                     :git.branch                       "coe-resources",
+                                                                     :git.commit.id                    "24bb0659461896b22a4a8b675a30b011bbf4efe4",
+                                                                     :org.opencontainers.image.title   "NuvlaEdge",
                                                                      :org.opencontainers.image.description
                                                                      "Common image for NuvlaEdge software components",
-                                                                     :git.run.number "839"},
+                                                                     :git.run.number                   "839"},
                                                                     :SharedSize -1,
-                                                                    :Size 192121737,
+                                                                    :Size       192121737,
                                                                     :Id
                                                                     "sha256:b4a4526cfd461c7bd1ad3b3e864b9a3f671890b2c42ea0cbad55dd999ab6ae9c",
                                                                     :Containers -1,
-                                                                    :ParentId "",
-                                                                    :Created 1726074087,
-                                                                    :RepoTags ["nuvladev/nuvlaedge:coe-resources"]}
+                                                                    :ParentId   "",
+                                                                    :Created    1726074087,
+                                                                    :RepoTags   ["nuvladev/nuvlaedge:coe-resources"]}
                                                                    {:RepoDigests
                                                                     ["nuvladev/nuvlaedge@sha256:33426aed6440dccdd36e75b5a46073d0888295496c17e2afcdddb51539ea7b99"],
                                                                     :Labels
-                                                                    {:git.build.time "$(date --utc +%FT%T.%3NZ)",
-                                                                     :org.opencontainers.image.vendor "SixSq SA",
+                                                                    {:git.build.time                   "$(date --utc +%FT%T.%3NZ)",
+                                                                     :org.opencontainers.image.vendor  "SixSq SA",
                                                                      :org.opencontainers.image.created "$(date --utc +%FT%T.%3NZ)",
-                                                                     :git.run.id "10746857192",
+                                                                     :git.run.id                       "10746857192",
                                                                      :org.opencontainers.image.url
                                                                      "https://github.com/nuvlaedge/nuvlaedge",
                                                                      :org.opencontainers.image.authors "support@sixsq.com",
-                                                                     :git.branch "coe-resources",
-                                                                     :git.commit.id "46a2ba7903ee7a1faa54b2aba9e283242c1bee5a",
-                                                                     :org.opencontainers.image.title "NuvlaEdge",
+                                                                     :git.branch                       "coe-resources",
+                                                                     :git.commit.id                    "46a2ba7903ee7a1faa54b2aba9e283242c1bee5a",
+                                                                     :org.opencontainers.image.title   "NuvlaEdge",
                                                                      :org.opencontainers.image.description
                                                                      "Common image for NuvlaEdge software components",
-                                                                     :git.run.number "836"},
+                                                                     :git.run.number                   "836"},
                                                                     :SharedSize -1,
-                                                                    :Size 191903136,
+                                                                    :Size       191903136,
                                                                     :Id
                                                                     "sha256:bd1e8ef984a199d31d3fc478431165ca0236176ad62fab2a4e68a2c5b8e12fbd",
                                                                     :Containers -1,
-                                                                    :ParentId "",
-                                                                    :Created 1725667915,
-                                                                    :RepoTags []}
+                                                                    :ParentId   "",
+                                                                    :Created    1725667915,
+                                                                    :RepoTags   []}
                                                                    {:RepoDigests
                                                                     ["nuvladev/nuvlaedge@sha256:2d92c970a5d8ce3e2fae5b88bb4d2a2cf701b0cdd4aa41e883aea79cd3e61859"],
                                                                     :Labels
-                                                                    {:git.build.time "$(date --utc +%FT%T.%3NZ)",
-                                                                     :org.opencontainers.image.vendor "SixSq SA",
+                                                                    {:git.build.time                   "$(date --utc +%FT%T.%3NZ)",
+                                                                     :org.opencontainers.image.vendor  "SixSq SA",
                                                                      :org.opencontainers.image.created "$(date --utc +%FT%T.%3NZ)",
-                                                                     :git.run.id "10746651311",
+                                                                     :git.run.id                       "10746651311",
                                                                      :org.opencontainers.image.url
                                                                      "https://github.com/nuvlaedge/nuvlaedge",
                                                                      :org.opencontainers.image.authors "support@sixsq.com",
-                                                                     :git.branch "coe-resources",
-                                                                     :git.commit.id "109a34446f5edf006fb1400ca266490492bf7363",
-                                                                     :org.opencontainers.image.title "NuvlaEdge",
+                                                                     :git.branch                       "coe-resources",
+                                                                     :git.commit.id                    "109a34446f5edf006fb1400ca266490492bf7363",
+                                                                     :org.opencontainers.image.title   "NuvlaEdge",
                                                                      :org.opencontainers.image.description
                                                                      "Common image for NuvlaEdge software components",
-                                                                     :git.run.number "835"},
+                                                                     :git.run.number                   "835"},
                                                                     :SharedSize -1,
-                                                                    :Size 191903117,
+                                                                    :Size       191903117,
                                                                     :Id
                                                                     "sha256:0ec61197db8b0989753da0c499be52b48c5d746a7d675ae358e157912d7d47bb",
                                                                     :Containers -1,
-                                                                    :ParentId "",
-                                                                    :Created 1725666894,
-                                                                    :RepoTags []}]
+                                                                    :ParentId   "",
+                                                                    :Created    1725666894,
+                                                                    :RepoTags   []}]
                                                                   )
                                  ::!default-columns       (r/atom [:Id :Size :Created])
                                  ::set-current-columns-fn #(reset! !current-columns %)
--- code/test/component/table.spec.ts
+++ code/test/component/table.spec.ts
@@ -1,32 +1,57 @@
 import { test, expect } from '@playwright/test';
 import { gotoScene } from './utils';
 
+function expectHeadersOrder(table, headers) {
+  expect(table.first().locator('thead tr th')).toHaveCount(headers.length);
+  headers.map((header, index) => {
+      expect(table.first().locator('thead tr th').nth(index)).toHaveText(header);
+  });
+}
+
+async function openColumnSelectorModal(sceneRoot) {
+  await sceneRoot.getByTitle('Columns selector').locator('i').click();
+}
+
+
+
 test('test', async ({ page }, { config }) => {
   const sceneRoot = await gotoScene(config, page, 'table-refactor-scenes', 'table-refactor');
 
   const table = await sceneRoot.locator('table.ui');
   expect(table).toHaveCount(1);
-  expect(table.first().locator('thead tr th').first()).toHaveText('Id');
-  expect(table.first().locator('thead tr th').nth(1)).toHaveText('Size');
-  expect(table.first().locator('thead tr th').nth(2)).toHaveText('Created');
+
+  expectHeadersOrder(table, ['Id', 'Size', 'Created']);
 
   expect(table.first().locator('thead tr')).toHaveCount(1);
   expect(table.first().locator('tbody tr')).toHaveCount(3);
-  expect(table.first().getByRole('cell', { name: 'Id' })).toBeVisible();
 
-  await sceneRoot.getByTitle('Columns selector').locator('i').click();
+//   Modal unselect Created column
+  openColumnSelectorModal(sceneRoot);
   await sceneRoot.locator('label').filter({ hasText: 'Created' }).click();
   await sceneRoot.getByLabel('update').click();
-  expect(table.first().locator('thead tr th')).toHaveCount(2);
-  
-  expect(table.first().locator('thead tr th').first()).toHaveText('Id');
-  expect(table.first().locator('thead tr th').nth(1)).toHaveText('Size');
+  expectHeadersOrder(table, ['Id', 'Size']);
 
-  // await page.frameLocator('iframe[title="Component scene"]').frameLocator('iframe').getByLabel('update').click();
-  // table.first().locator('thead tr td').count();
-//     await expect(page.frameLocator('iframe[title="Component scene"]').frameLocator('iframe').getByRole('cell', { name: 'Id' })).toBeVisible();
-//     await expect(page.frameLocator('iframe[title="Component scene"]').frameLocator('iframe').getByRole('cell', { name: 'Size' })).toBeVisible();
-//     await expect(page.frameLocator('iframe[title="Component scene"]').frameLocator('iframe').getByRole('cell', { name: 'Created' })).toBeVisible();
+// Modal select defaults columns and cancel
+  openColumnSelectorModal(sceneRoot);
+  await sceneRoot.getByLabel('Select default columns').click();
+  await sceneRoot.getByLabel('cancel').click();
+  expectHeadersOrder(table, ['Id', 'Size']);
+
+// Modal select defaults columns and cancel
+  openColumnSelectorModal(sceneRoot);
+  await sceneRoot.getByLabel('Select default columns').click();
+  await sceneRoot.getByLabel('update').click();
+  expectHeadersOrder(table, ['Id', 'Size', 'Created']);
+
+// Expect delete Id column is not possible
+  await table.first().getByRole('cell', { name: 'Id' }).hover();
+  await expect(table.first().getByRole('cell', { name: 'Id' }).locator('a[aria-label="Delete Column"]')).toHaveCount(0);
+
+// Expect delete Size column is possible
+  await table.first().getByRole('cell', { name: 'Size' }).hover();
+  await table.first().getByRole('cell', { name: 'Size' }).locator('a[aria-label="Delete Column"]').click();
+  expectHeadersOrder(table, ['Id', 'Created']);
+
 
 });
 

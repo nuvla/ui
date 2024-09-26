59f03c1eb9c09f742f39627d2d18206fe3dd74e9
Alessandro Bellucci <alessandro.bellucci@sixsq.com>
2024-09-26 09:40:52 +0200

    fix(portfolio): add component code in code preview pane

--- code/portfolio/src/sixsq/nuvla/ui/portfolio_utils.clj
+++ code/portfolio/src/sixsq/nuvla/ui/portfolio_utils.clj
@@ -5,13 +5,14 @@
 
 (defmacro defscene [id & body]
   `(do (p/defscene ~id
-         [:iframe.scene-canvas {:src   (str "/scene.html?id=" ~(name id))
-                                :style {:border           :none
-                                        :flex-grow        1
-                                        :width            "100%"
-                                        :min-height       "500px"
-                                        :background-color "rgb(255, 255, 255)"
-                                        :background-image :none}}])
+         [:iframe.scene-canvas {:src      (str "/scene.html?id=" ~(name id))
+                                :style    {:border           :none
+                                           :flex-grow        1
+                                           :width            "100%"
+                                           :min-height       "500px"
+                                           :background-color "rgb(255, 255, 255)"
+                                           :background-image :none}
+                                :data-src '~@body}])
        (defn ~id
          []
          ~@body)

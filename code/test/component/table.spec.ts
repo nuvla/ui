ff44cc70c219910770220a05283b18c2ded761b9
Alessandro Bellucci <alessandro.bellucci@sixsq.com>
2024-09-27 10:40:39 +0200

    Test on sorting

--- code/test/component/table.spec.ts
+++ code/test/component/table.spec.ts
@@ -26,7 +26,7 @@ test('test', async ({ page }, { config }) => {
   const table = await sceneRoot.locator('table.ui');
   expect(table).toHaveCount(1);
 
-  expectHeadersOrder(table, ['Id', 'Size', 'Created']);
+  await expectHeadersOrder(table, ['Id', 'Size', 'Created']);
 
   expect(table.first().locator('thead tr')).toHaveCount(1);
   expect(table.first().locator('tbody tr')).toHaveCount(3);
@@ -35,19 +35,19 @@ test('test', async ({ page }, { config }) => {
   openColumnSelectorModal(sceneRoot);
   await sceneRoot.locator('label').filter({ hasText: 'Created' }).click();
   await sceneRoot.getByLabel('update').click();
-  expectHeadersOrder(table, ['Id', 'Size']);
+  await expectHeadersOrder(table, ['Id', 'Size']);
 
 // Modal select defaults columns and cancel
   openColumnSelectorModal(sceneRoot);
   await sceneRoot.getByLabel('Select default columns').click();
   await sceneRoot.getByLabel('cancel').click();
-  expectHeadersOrder(table, ['Id', 'Size']);
+  await expectHeadersOrder(table, ['Id', 'Size']);
 
 // Modal select defaults columns and cancel
   openColumnSelectorModal(sceneRoot);
   await sceneRoot.getByLabel('Select default columns').click();
   await sceneRoot.getByLabel('update').click();
-  expectHeadersOrder(table, ['Id', 'Size', 'Created']);
+  await expectHeadersOrder(table, ['Id', 'Size', 'Created']);
 
 // Expect delete Id column is not possible
   await table.first().getByRole('cell', { name: 'Id' }).hover();
@@ -56,7 +56,7 @@ test('test', async ({ page }, { config }) => {
 // Expect delete Size column is possible
   await table.first().getByRole('cell', { name: 'Size' }).hover();
   await table.first().getByRole('cell', { name: 'Size' }).locator('a[aria-label="Delete Column"]').click();
-  expectHeadersOrder(table, ['Id', 'Created']);
+  await expectHeadersOrder(table, ['Id', 'Created']);
 
 // Sort by Created ascending
   await table.first().getByRole('cell', { name: 'Created' }).click();

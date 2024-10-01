1d000ec7f028eee3c31b050cc0af5bb3c5e8f187
Alessandro Bellucci <alessandro.bellucci@sixsq.com>
2024-10-01 16:05:22 +0200

    Qualified scene id in portfolio frame parameters

--- code/portfolio/src/sixsq/nuvla/ui/portfolio_utils.clj
+++ code/portfolio/src/sixsq/nuvla/ui/portfolio_utils.clj
@@ -4,18 +4,20 @@
 (defmulti scene-root identity)
 
 (defmacro defscene [id & body]
-  `(do (p/defscene ~id
-         [:iframe.scene-canvas {:src      (str "/scene.html?id=" ~(name id))
-                                :style    {:border           :none
-                                           :flex-grow        1
-                                           :width            "100%"
-                                           :min-height       "500px"
-                                           :background-color "rgb(255, 255, 255)"
-                                           :background-image :none}
-                                :data-src '~@body}])
-       (defn ~id
-         []
-         ~@body)
-       (defmethod scene-root ~(name id)
-         []
-         [~id])))
+  (let [scene-id  (str (-> &env :ns :name) "." (name id))
+        scene-url (str "/scene.html?id=" scene-id)]
+    `(do (p/defscene ~id
+           [:iframe.scene-canvas {:src      ~scene-url
+                                  :style    {:border           :none
+                                             :flex-grow        1
+                                             :width            "100%"
+                                             :min-height       "500px"
+                                             :background-color "rgb(255, 255, 255)"
+                                             :background-image :none}
+                                  :data-src '~@body}])
+         (defn ~id
+           []
+           ~@body)
+         (defmethod scene-root ~scene-id
+           []
+           [~id]))))

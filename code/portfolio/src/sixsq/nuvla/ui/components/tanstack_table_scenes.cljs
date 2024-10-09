06fd8ad4571b466f4e54bc0905da3a660ff9dcda
khaled basbous <khaled@sixsq.com>
2024-10-09 16:53:17 +0200

    remove tanstack/table

--- code/portfolio/src/sixsq/nuvla/ui/components/tanstack_table_scenes.cljs
+++
@@ -1,7 +0,0 @@
-(ns sixsq.nuvla.ui.components.tanstack-table-scenes
-  (:require [sixsq.nuvla.ui.portfolio-utils :refer [defscene]]
-            [sixsq.nuvla.ui.common-components.plugins.tanstack-table :refer [Table]]))
-
-(defscene table
-  Table)
-

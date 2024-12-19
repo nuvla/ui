4831965416742d824ed56575e18f3753d731d2f3
Khaled Basbous <khaled@sixsq.com>
2024-12-19 21:00:15 +0100

    try page screenshot if more stable (#1706)

--- code/test/component/job-cell-coe-resource-actions-multiple.spec.ts
+++ code/test/component/job-cell-coe-resource-actions-multiple.spec.ts
@@ -4,8 +4,8 @@ import { expectShortMsg } from './job-cell';
 
 test('coe-resource-actions-multiple', async ({ page }, { config }) => {
   const sceneRoot = await gotoScene(config, page, 'job-cell-scenes', 'coe-resource-actions-multiple');
-  await expect(sceneRoot).toHaveScreenshot();
+  await expect(page).toHaveScreenshot();
   await sceneRoot.getByRole('button', { name: '▼' }).click();
-  await expect(sceneRoot).toHaveScreenshot();
+  await expect(page).toHaveScreenshot();
   await expect(sceneRoot.getByRole('button', { name: '▲' })).toBeVisible();
-  await expect(sceneRoot).toHaveScreenshot();});
+  await expect(page).toHaveScreenshot();});

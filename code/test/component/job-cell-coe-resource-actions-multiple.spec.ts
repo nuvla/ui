efe11de6a0b85c83a01a12928cf9b40abe7dbd0f
khaled basbous <khaled@sixsq.com>
2025-01-23 16:38:17 +0100

    fix(Components test): Screenshot timeout extending because rendering is slow on github action

--- code/test/component/job-cell-coe-resource-actions-multiple.spec.ts
+++ code/test/component/job-cell-coe-resource-actions-multiple.spec.ts
@@ -4,8 +4,9 @@ import { expectShortMsg } from './job-cell';
 
 test('coe-resource-actions-multiple', async ({ page }, { config }) => {
   const sceneRoot = await gotoScene(config, page, 'job-cell-scenes', 'coe-resource-actions-multiple');
-  await expect(sceneRoot).toHaveScreenshot();
+  await expect(sceneRoot).toHaveScreenshot({ timeout: 10000 });
   await sceneRoot.getByRole('button', { name: '▼' }).click();
-  await expect(sceneRoot).toHaveScreenshot();
+  await expect(sceneRoot).toHaveScreenshot({ timeout: 10000 });
   await expect(sceneRoot.getByRole('button', { name: '▲' })).toBeVisible();
-  await expect(sceneRoot).toHaveScreenshot();});
+  await expect(sceneRoot).toHaveScreenshot({ timeout: 10000 });
+  });

6aabf4162e959f39ef7c1ec7cd274a983dd99f2b
khaled basbous <khaled@sixsq.com>
2025-01-24 11:18:50 +0100

    attempt to screenshot root to avoid vertical shift

--- code/test/component/bulk-progress-monitored-job-scenes.spec.ts
+++ code/test/component/bulk-progress-monitored-job-scenes.spec.ts
@@ -4,10 +4,11 @@ import { expectMsg } from './job-cell';
 
 test('monitored-job-without-progress', async ({ page }, { config }) => {
   const sceneRoot = await gotoScene(config, page, 'bulk-progress-monitored-job-scenes', 'monitored-job-without-progress');
+  const rootLocator = await sceneRoot.locator('#root');
   await expect(sceneRoot.getByRole('button', { name: 'Reset' })).toBeVisible();
-  await expect(sceneRoot).toHaveScreenshot();
+  await expect(rootLocator).toHaveScreenshot();
   await sceneRoot.getByRole('cell', { name: 'Offline Edges' }).locator('div').first().click();
-  await expect(sceneRoot).toHaveScreenshot();
+  await expect(rootLocator).toHaveScreenshot();
   await sceneRoot.getByRole('cell', { name: 'Error reason foobar' }).locator('div').first().click();
-  await expect(sceneRoot).toHaveScreenshot();
+  await expect(rootLocator).toHaveScreenshot();
 });
--- code/test/component/job-cell-coe-resource-actions-multiple.spec.ts
+++ code/test/component/job-cell-coe-resource-actions-multiple.spec.ts
@@ -4,10 +4,11 @@ import { expectShortMsg } from './job-cell';
 
 test('coe-resource-actions-multiple', async ({ page }, { config }) => {
   const sceneRoot = await gotoScene(config, page, 'job-cell-scenes', 'coe-resource-actions-multiple');
+  const rootLocator = await sceneRoot.locator('#root');
   await expect(sceneRoot.getByRole('button', { name: 'Reset' })).toBeVisible();
-  await expect(sceneRoot).toHaveScreenshot();
+  await expect(rootLocator).toHaveScreenshot();
   await sceneRoot.getByRole('button', { name: '▼' }).click();
-  await expect(sceneRoot).toHaveScreenshot();
+  await expect(rootLocator).toHaveScreenshot();
   await expect(sceneRoot.getByRole('button', { name: '▲' })).toBeVisible();
-  await expect(sceneRoot).toHaveScreenshot();
+  await expect(rootLocator).toHaveScreenshot();
   });

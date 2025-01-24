92f4df68de29209cbf29117151975ec5d6d281cf
Khaled Basbous <khaled@sixsq.com>
2025-01-24 13:47:24 +0100

    fix(Components test): Reduce workers to 2 to avoid flakyness (#1722)
    fix(Component tests): Wait reset button to be visible before taking screenshots
    fix(CI): Deprecated github upload-artifact@v3 upgraded to upload-artifact@v4

--- code/test/component/bulk-progress-monitored-job-scenes.spec.ts
+++ code/test/component/bulk-progress-monitored-job-scenes.spec.ts
@@ -4,6 +4,7 @@ import { expectMsg } from './job-cell';
 
 test('monitored-job-without-progress', async ({ page }, { config }) => {
   const sceneRoot = await gotoScene(config, page, 'bulk-progress-monitored-job-scenes', 'monitored-job-without-progress');
+  await expect(sceneRoot.getByRole('button', { name: 'Reset' })).toBeVisible();
   await expect(sceneRoot).toHaveScreenshot();
   await sceneRoot.getByRole('cell', { name: 'Offline Edges' }).locator('div').first().click();
   await expect(sceneRoot).toHaveScreenshot();

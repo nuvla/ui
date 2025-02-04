9e5a41b4851556cf92c646706e9f4102e735baf7
khaled basbous <khaled@sixsq.com>
2025-02-04 10:22:19 +0100

    go back to working versions

--- code/project.clj
+++ code/project.clj
@@ -59,7 +59,7 @@
 
   :profiles
   {:dev       {:dependencies [[org.clojure/clojure "1.11.1"]
-                              [org.clojure/clojurescript "1.11.132"
+                              [org.clojure/clojurescript "1.11.60"
                                :exclusions
                                [com.google.javascript/closure-compiler-unshaded
                                 org.clojure/google-closure-library
@@ -68,10 +68,14 @@
                               [day8.re-frame/re-frame-10x "1.9.10"]
                               [com.github.ljpengelen/shadow-cljs-hash-assets-hook "1.1.0"]]}
 
-   :scljs     {:dependencies [[thheller/shadow-cljs "2.28.20"] ;; WARNING: also in package.json
-                              [org.clojure/google-closure-library "0.0-20230227-c7c0a541"]
-                              [org.clojure/google-closure-library-third-party "0.0-20230227-c7c0a541"]
-                              [com.google.javascript/closure-compiler-unshaded "v20240317"]
+   :scljs     {:dependencies [
+                              ;; Don't go beyond shadow-cljs v2.19.9 because it will stop working on Cloudflare
+                              ;; which removed support of java from their v2 works.
+                              ;; Maybe building in Github and push to cloudflare pages. Something to check.
+                              [thheller/shadow-cljs "2.19.9"] ;; WARNING: also in package.json
+                              [org.clojure/google-closure-library "0.0-20211011-0726fdeb"]
+                              [org.clojure/google-closure-library-third-party "0.0-20211011-0726fdeb"]
+                              [com.google.javascript/closure-compiler-unshaded "v20220803"]
                               [djblue/portal "0.58.5"]]}
 
    :portfolio {:dependencies [[no.cjohansen/portfolio "2025.01.28"]]

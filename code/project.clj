b59cbbd6e415074acff8e33f2c90f24ab049676e
Khaled Basbous <khaled@sixsq.com>
2025-02-05 15:23:38 +0100

    fix(CI): Build dependencies upgrade (#1728)

--- code/project.clj
+++ code/project.clj
@@ -39,20 +39,20 @@
   :dependencies [[reagent "1.2.0" :scope "provided"
                   :exclusions [cljsjs/react
                                cljsjs/react-dom]]
-                 [re-frame "1.3.0" :scope "provided"]
+                 [re-frame "1.4.3" :scope "provided"]
                  [day8.re-frame/http-fx "0.2.4" :scope "provided"]
                  [cljs-ajax "0.8.4" :scope "provided"]
                  [expound "0.9.0" :scope "provided"]
-                 [com.taoensso/timbre "6.1.0" :scope "provided"]
+                 [com.taoensso/timbre "6.6.1" :scope "provided"]
                  [sixsq.nuvla/api ~sixsq-nuvla-api-version :scope "provided"]
-                 [com.taoensso/tempura "1.5.3" :scope "provided"]
+                 [com.taoensso/tempura "1.5.4" :scope "provided"]
                  [com.cemerick/url "0.1.1" :scope "provided"]
                  [kwladyka/form-validator-cljs "1.2.1" :scope "provided"]
-                 [instaparse "1.4.14" :scope "provided"]
+                 [instaparse "1.5.0" :scope "provided"]
                  [com.degel/re-frame-storage-fx "0.1.1" :scope "provided"]
                  [markdown-to-hiccup "0.6.2" :scope "provided"]
                  [clj-kondo "RELEASE" :scope "provided"]
-                 [metosin/reitit "0.7.1"]]
+                 [metosin/reitit "0.7.2"]]
 
   :source-paths ["src/clj" "src/cljs"]
   :test-paths ["test/cljs"]
@@ -65,16 +65,20 @@
                                 org.clojure/google-closure-library
                                 org.clojure/google-closure-library-third-party]]
                               [binaryage/devtools "1.0.7" :scope "test"]
-                              [day8.re-frame/re-frame-10x "1.6.0"]
+                              [day8.re-frame/re-frame-10x "1.9.10"]
                               [com.github.ljpengelen/shadow-cljs-hash-assets-hook "1.1.0"]]}
 
-   :scljs     {:dependencies [[thheller/shadow-cljs "2.17.8"] ;; WARNING: also in package.json
+   :scljs     {:dependencies [
+                              ;; Don't go beyond shadow-cljs v2.17.8 because it will stop working on Cloudflare
+                              ;; which removed support of java from their v2 works.
+                              ;; Maybe building in Github and push to cloudflare pages. Something to check.
+                              [thheller/shadow-cljs "2.17.8"] ;; WARNING: also in package.json
                               [org.clojure/google-closure-library "0.0-20211011-0726fdeb"]
                               [org.clojure/google-closure-library-third-party "0.0-20211011-0726fdeb"]
                               [com.google.javascript/closure-compiler-unshaded "v20220202"]
-                              [djblue/portal "0.42.1"]]}
+                              [djblue/portal "0.58.5"]]}
 
-   :portfolio {:dependencies [[no.cjohansen/portfolio "2024.03.18"]]
+   :portfolio {:dependencies [[no.cjohansen/portfolio "2025.01.28"]]
                :source-paths ["portfolio/src"]}}
 
 
